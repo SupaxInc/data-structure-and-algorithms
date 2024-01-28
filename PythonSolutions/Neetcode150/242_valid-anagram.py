088c87e2a2b8c2de995f17ac8ea6f96dba8c316e
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-01-28 18:03:54 -0500

    modified 242

--- PythonSolutions/Neetcode150/242_valid-anagram.py
+++ PythonSolutions/Neetcode150/242_valid-anagram.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def countLetters(self, s: str) -> dict:
         letterMap = {}
 
@@ -14,9 +14,26 @@ class Solution:
         sMap = self.countLetters(s)
         tMap = self.countLetters(t)
 
+        # In python you can compare sets with another set easily due to built in operators
+        if set(sMap.keys()) != set(tMap.keys()):
+            return False
+        
         for key in sMap:
             if sMap[key] != tMap[key]:
                 return False
         
         return True
     
+class Solution2:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        # You can also compare hashmaps with another hashmap, it compares both keys and values
+        return countS == countT
