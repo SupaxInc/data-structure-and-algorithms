f702d1b78de81fa58367ee19c6257144b7e6e1ce
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-01-28 15:37:24 -0500

    solved 217

--- PythonSolutions/Neetcode150/217_contains-duplicate.py
+++ PythonSolutions/Neetcode150/217_contains-duplicate.py
@@ -1,1 +1,14 @@
-print('test')
+from typing import List
+
+def containsDuplicate(nums: List[int]) -> bool:
+    mySet = set()
+
+    for n in nums:
+        if n in mySet:
+            return True
+        
+        mySet.add(n)
+    
+    return False
+
+print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
