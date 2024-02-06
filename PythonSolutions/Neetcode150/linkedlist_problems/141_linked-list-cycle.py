a312312271505e90ac46fdd43053ab35e7735cff
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-02-05 21:27:35 -0500

    solved 141

---
+++ PythonSolutions/Neetcode150/linkedlist_problems/141_linked-list-cycle.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class MySolution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+
+        # Goes through the first iteration
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            # Check if there's a cycle for the beginning of each iteration
+            if slow == fast:
+                return True
+
+            slow = slow.next
+            # Fast node goes twice as fast as the slow node
+            fast = fast.next.next
+        
+        return False
