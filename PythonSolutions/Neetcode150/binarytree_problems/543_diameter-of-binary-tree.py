7b0a366d12e0540d45a74665e0c93cc14eb71435
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-02-10 17:43:40 -0500

    fixed 543

--- PythonSolutions/Neetcode150/binarytree_problems/543_diameter-of-binary-tree.py
+++ PythonSolutions/Neetcode150/binarytree_problems/543_diameter-of-binary-tree.py
@@ -9,15 +9,20 @@ class Solution:
         maxDiameter = 0
 
         def dfs(node):
+            # Use non local to use variables from the parent scope
             nonlocal maxDiameter
             if not node:
                 return 0
             
             left = dfs(node.left)
             right = dfs(node.right)
-            maxDiameter = max(maxDiameter, left + right)  # Update to consider the diameter through the current node
+            # When the stack pops for each left and right sub tree
+            # We can add the max depth of both sub trees to get the max diameter
+            maxDiameter = max(maxDiameter, left + right) 
 
-            return 1 + max(left, right)  # Return the height of the current subtree
+            # Return the max depth of the current subtree
+            # Adds a 1 to add for the height of the current node continously
+            return 1 + max(left, right)  
         
         dfs(root)
         return maxDiameter
