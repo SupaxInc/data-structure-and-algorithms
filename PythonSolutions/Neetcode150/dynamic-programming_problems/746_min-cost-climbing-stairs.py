983589b2d447092c44571517b928b55f42417cf6
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-02-17 23:58:56 -0500

    updating 746 to class

--- PythonSolutions/Neetcode150/dynamic-programming_problems/746_min-cost-climbing-stairs.py
+++ PythonSolutions/Neetcode150/dynamic-programming_problems/746_min-cost-climbing-stairs.py
@@ -1,10 +1,11 @@
-def minCostClimbingStairs(cost):
-    n = len(cost)
-    dp = [0] * (n + 1)  # +1 to accommodate reaching the top past the last step
-    dp[0], dp[1] = cost[0], cost[1]
-    
-    for i in range(2, n):
-        dp[i] = cost[i] + min(dp[i-1], dp[i-2])
-    
-    # Compare the last two steps to decide the minimum cost to reach the top
-    return min(dp[n-1], dp[n-2])
+class BottomUpSolution:
+    def minCostClimbingStairs(cost):
+        n = len(cost)
+        dp = [0] * (n + 1)  # +1 to accommodate reaching the top past the last step
+        dp[0], dp[1] = cost[0], cost[1]
+        
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+        
+        # Compare the last two steps to decide the minimum cost to reach the top
+        return min(dp[n-1], dp[n-2])
