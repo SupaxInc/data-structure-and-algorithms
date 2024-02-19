ad4575f712a138462fed1a4bcb780d3371c8e1b2
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-02-19 17:11:04 -0500

    updated 268 to be more readable

--- PythonSolutions/Neetcode150/bitmanipulation_problems/268_missing-number.py
+++ PythonSolutions/Neetcode150/bitmanipulation_problems/268_missing-number.py
@@ -1,6 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        xor = len(nums)
-        for i in range(len(nums)):
-            xor ^= i ^ nums[i]
-        return xor
+        # Begin at 0 to cancel out num 0
+        res = 0
+        # Iterate through the array plus the max of array length
+        # Helps cover all 0 to n numbers
+        for i in range(0, len(nums) + 1):
+            # Cancel out all nums in current array and prevent from accessing array out of bounds
+            if i < len(nums): 
+                res ^= nums[i]
+            # Cancel out all nums in index, 0 to n
+            res ^= i
+        
+        return res
