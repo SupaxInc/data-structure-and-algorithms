c634d1b4cb6165156597169c6c63e80a553f6a45
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-03-06 14:55:07 -0500

    added an improved readable solution for interviews

--- PythonSolutions/Neetcode150/linkedlist_problems/143_reorder-list.py
+++ PythonSolutions/Neetcode150/linkedlist_problems/143_reorder-list.py
@@ -3,6 +3,64 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+class BetterReadableSolution:
+    def reorderList(self, head) -> ListNode:
+        if not head or not head.next:
+            return
+        # Ask if there are constraints (can we maybe just change the values?)
+        # PSEUDO CODE
+        # find midway point -
+        # slow and fast pointer
+        # second = slow.next
+        # slow.next = None
+
+        # Reverse second list -
+        # currnode, prevnode = second, None 
+        # temp = curr.next
+        # curr.next = prevnode
+        # prevnode = curr
+        # curr = temp
+
+        # merge two lists - 
+        # first, second = head, prev
+        # 1 -> 2 -> 3 , 5 -> 4
+        # 1 -> 5 -> 2 -> 4 -> 3
+
+        # Find mid point of the list
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Create second list using mid point
+        second = slow.next
+        # Point the memory on mid point of head to None so it no longer points to new second list
+        slow.next = None 
+
+        # Reverse second list
+        currNode, prevNode = second, None
+        while currNode:
+            temp = currNode.next
+            currNode.next = prevNode
+            prevNode = currNode # prevNode is now the second list
+            currNode = temp
+        
+        # Merge two lists
+        # 1 -> 2 -> 3 , 5 -> 4
+        # 1 -> 5 -> 2 -> 4 -> 3
+        first, second = head, prevNode
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second # First list points to the second list
+            second.next = temp1 # Second list points to the next value of first list, creates a 1 -> 5 -> 2
+            first, second = temp1, temp2
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseList(self, head) -> ListNode:
         currNode, prevNode = head, None
