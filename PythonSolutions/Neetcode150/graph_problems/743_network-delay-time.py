467de620727ef8e1d0ca631140ee234b4982c270
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-03-31 21:24:19 -0400

    solved 743

---
+++ PythonSolutions/Neetcode150/graph_problems/743_network-delay-time.py
@@ -0,0 +1,36 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+
+        # Create a DIRECTED graph
+        for source, target, weight in times:
+            graph[source].append((target, weight))
+        
+        # Set the current distance of all vertices from 1 to n
+        vertexTimes = { vertex: float("inf") for vertex in range(1, n+1) }
+        # Set the source node K, to 0 since time to a source would just be 0
+        vertexTimes[k] = 0
+
+        minHeap = [(0, k)] # (current time, current selected node)
+        visited = set()
+
+        while minHeap:
+            currTime, currNode = heapq.heappop(minHeap)
+            if currNode in visited: continue
+
+            visited.add(currNode)
+
+            for nei, time in graph[currNode]:
+                newTime = currTime + time
+                # Only store the new time that is shorter than current neighbors shortest time path
+                if newTime < vertexTimes[nei]:
+                    vertexTimes[nei] = newTime
+                    heapq.heappush(minHeap, (newTime, nei))
+        
+        # The max time path is the answer
+            # We are always looking for the shortest time path, and the max time path will give us the answer
+            # To find that all nodes received the signal
+        maxTime = max(vertexTimes.values())
+
+        return maxTime if maxTime < float("inf") else -1
+
