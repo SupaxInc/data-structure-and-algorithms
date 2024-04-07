f736dbabfaac97fb8f242f0d05c1dc1e9d729bc9
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-04-06 23:50:03 -0400

    solved 198

---
+++ PythonSolutions/Neetcode150/dynamic-programming_problems/198_house-robber.py
@@ -0,0 +1,34 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # If the list is empty, there's nothing to rob.
+        if not nums: 
+            return 0
+        # If there are 1 or 2 houses, simply return the maximum value
+        # available, since you can only choose from these without any
+        # adjacent restrictions.
+        elif len(nums) <= 2:
+            return max(nums)
+
+        # Initialize two variables to store the maximum amount of money
+        # that can be robbed up to the previous house considering adjacency
+        # (prevAdj) and the house before that (prevNonAdj).
+        prevAdj, prevNonAdj = max(nums[0], nums[1]), nums[0]
+
+        # Start looping from the third house (index 2) onwards.
+        for i in range(2, len(nums)):
+            # Calculate the current maximum amount that can be robbed.
+            # It's either robbing the current house plus the amount from
+            # two houses ago (for non-adjacency) or the amount from the
+            # previous step (maintaining adjacency restrictions).
+            currentMax = max(nums[i] + prevNonAdj, prevAdj)
+            
+            # Update our two tracking variables for the next iteration.
+            # The current maximum becomes the new prevAdj (since we're
+            # moving to the next house), and the previous prevAdj becomes
+            # the new prevNonAdj (as it's now two steps back).
+            prevAdj, prevNonAdj = currentMax, prevAdj
+        
+        # After going through all the houses, prevAdj holds the maximum
+        # amount of money that can be robbed considering the adjacency
+        # constraint.
+        return prevAdj
