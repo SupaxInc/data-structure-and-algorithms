402e54f7735ccbde8545bde046bda6dcfe7e6c25
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-04-13 16:10:08 -0400

    solved 139 with optimized dp solution

--- PythonSolutions/Neetcode150/dynamic-programming_problems/139_word-break.py
+++ PythonSolutions/Neetcode150/dynamic-programming_problems/139_word-break.py
@@ -17,3 +17,25 @@ class BruteForceSolution:
     s = "pineapple"
     wordDict = ["pine", "app", "apple"]
     print(wordBreak(s, wordDict))  # Output: True
+
+class OptimizedSolution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s) + 1
+        wordDict = set(wordDict) 
+
+        dp = [False] * n
+        # Base case, 1 way to segment empty string
+        dp[0] = True
+
+        for end in range(1, n):
+            # Partition the strings so we can find segments from start to end
+                # E.g. pineapp, ineapp, neapp, eapp, "app" is a segment
+            for start in range(end):
+                word = s[start:end]
+                # Check if the start of the partition dp[start] is cached 
+                    # which means there was a word there that was segmented
+                if dp[start] and word in wordDict:
+                    dp[end] = True
+                    break # Break since we no longer need to look for any more segments in partition
+
+        return dp[len(s)] 
