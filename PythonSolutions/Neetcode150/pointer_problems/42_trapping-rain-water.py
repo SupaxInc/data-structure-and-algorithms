b7d967de716333bbcf58f1c46e9a9402b9104360
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-05-09 15:13:15 -0400

    solved 42 with better optimization

--- PythonSolutions/Neetcode150/pointer_problems/42_trapping-rain-water.py
+++ PythonSolutions/Neetcode150/pointer_problems/42_trapping-rain-water.py
@@ -19,8 +19,37 @@ class Solution:
         
         # Get the units per height
         for i in range(n):
+            # Similar to container with most water, we need to get the min of the max heights
+                # This is so that the water does not spill over
             units = min(maxLefts[i], maxRights[i]) - height[i] # Formula to get units
             if units > 0:
                 total += units
+        
+        return total
+    
+class MostOptimizedSolution:
+    def trap(self, height: List[int]) -> int:
+        if len(height) < 2:
+            return 0
+
+        l, r = 0, len(height) - 1
+        maxL, maxR = height[l], height[r]
+        total = 0
+
+        while l < r:
+            # The pointer with the lower max height moves toward the other pointer
+                # This is so that the local height (current pointer height) has a higher chance
+                # of trapping water without spilling over.
+            if maxL < maxR:
+                l += 1 # Moving pointers right away prevents including calculation from beginning pointers
+                # Update max height seen right away
+                    # This might be unintuitive b/c max height might be current position
+                    # Then the total result would be 0 either way
+                maxL = max(maxL, height[l]) 
+                total += maxL - height[l] # Use left pointer as current height
+            else:
+                r -= 1
+                maxR = max(maxR, height[r])
+                total += maxR - height[r] # Use right pointer as current height
         
         return total
