b55ed5df2593d1b476167d4e87cf15f7dc09d339
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-05-14 21:53:39 -0400

    Found optmized solution for 239

--- PythonSolutions/Neetcode150/slidingwindow_problems/239_sliding-window-maximum.py
+++ PythonSolutions/Neetcode150/slidingwindow_problems/239_sliding-window-maximum.py
@@ -11,3 +11,31 @@ class BruteForceSolution:
                 break
         
         return res
+
+class OptimizedSolution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+
+        q = deque()
+        start = 0
+        for end in range(len(nums)):
+            # Remove elements that are not within the current fixed window
+                # q[0] is accessing the index NOT the value of the queue
+                # It is the index of the first element added to queue
+            if q and q[0] < start:
+                q.popleft() # Remove first element added to queue
+            
+            # Maintain descending order in current fixed window
+                # Compares last element added to queue with current element
+            while q and nums[q[-1]] < nums[end]:
+                q.pop() # Remove last element added to queue
+            
+            q.append(end) # Add the current index to queue after maintaining descending order
+
+            # If the window is of valid size then add to result
+            if (end - start + 1) == k:
+                # Add the first element in queue (the max since its descending order)
+                res.append(nums[q[0]])
+                start += 1
+        
+        return res
