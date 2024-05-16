789cd180a90c97b8cf7fff92c7e798b245953f22
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-05-15 21:02:11 -0400

    Clarified 150

--- PythonSolutions/Neetcode150/stack_problems/150_evaluate-reverse-polish-notation.py
+++ PythonSolutions/Neetcode150/stack_problems/150_evaluate-reverse-polish-notation.py
@@ -2,6 +2,7 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         for token in tokens:
+            # Can't use isdigit() as it only checks from 0-9
             if token not in "+-*/":
                 stack.append(int(token))
             else:
@@ -16,6 +17,6 @@ class Solution:
                 elif token == '/':
                     # Dividing left and right would create a floating point number
                     # We convert it to an int so that it truncates to 0 (e.g -3.5 to -3)
-                    # // would not work here as it truncates to -Infinity (e.g -3.5 to -4)
+                    # Using // would not work here as it truncates to -Infinity (e.g -3.5 to -4)
                     stack.append(int(left / right))
         return stack[0]
