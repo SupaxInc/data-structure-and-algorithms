f2bf65d2210a139a35e30abcfd9e0770775bdfcf
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-05-21 23:34:25 -0400

    Clarified 153

--- PythonSolutions/Neetcode150/binarysearch_problems/153_find-min-in-rotated-sorted-array.py
+++ PythonSolutions/Neetcode150/binarysearch_problems/153_find-min-in-rotated-sorted-array.py
@@ -7,12 +7,14 @@ class Solution:
             mid = lo + ((hi - lo) // 2)
             minNum = min(minNum, nums[mid])
 
-            # If the number in the middle is greater than the number at the end
-            # Then we search on the right side of the array
-            # This is because the array will be sorted in a way that the min numbers are on the right
-            if nums[mid] > nums[hi]:
+            # It is rotated in a way that smaller numbers are on the left of the bigger number in mid
+            if nums[mid] > nums[hi]: 
+                # So move lo pointer to the right to find smaller numbers
                 lo = mid + 1
+            # If mid smaller than end, then its rotated in a way that smaller numbers are on the left
             else:
+                # So move hi pointer to the left to find smaller numbers
                 hi = mid - 1
         
         return minNum
+        
