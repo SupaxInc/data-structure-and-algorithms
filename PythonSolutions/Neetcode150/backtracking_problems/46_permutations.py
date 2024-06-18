a3a17671a29af0bc0d404f1e4e5cdc60de0862ef
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-06-18 16:43:58 -0400

    Updating comments for 46

--- PythonSolutions/Neetcode150/backtracking_problems/46_permutations.py
+++ PythonSolutions/Neetcode150/backtracking_problems/46_permutations.py
@@ -6,7 +6,7 @@ class Solution:
             # When start hits same index as length of nums
                 # This means a complete permutation has formed
             if start == len(nums):
-                result.append(nums[:])
+                result.append(nums[:]) # Deep copy
                 return
 
             for i in range(start, len(nums)):
@@ -14,6 +14,10 @@ class Solution:
                 nums[start], nums[i] = nums[i], nums[start]
                 
                 # Explore current index choice as deep as possible 
+                    # Changes the range of the for loop from start + 1 to len(nums)
+                    # E.g. [1, 2, 3]
+                    # Initial -> Idx: 0 to 3 [1, 2, 3] -> Explore -> 1 to 3 [2, 3] -> 
+                    # Explore -> 2 to 3 [3] -> Explore -> 3 to 3 [] -> Hits Base Case 
                 backtrack(start + 1)
 
                 # Undo swap
