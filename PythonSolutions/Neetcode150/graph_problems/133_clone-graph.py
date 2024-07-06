61d42b5f832ec26916999e44bf52cdb27966f9d9
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-07-05 23:45:16 -0400

    Updated 133

--- PythonSolutions/Neetcode150/graph_problems/133_clone-graph.py
+++ PythonSolutions/Neetcode150/graph_problems/133_clone-graph.py
@@ -27,6 +27,12 @@ class Solution:
 
             # Go through all neighbors of old node so that we can recursively copy 
                 # the previously mapped nodes to neighbors of a newly copied node
+                # For example: 1 -> 2 -> 3
+                    # Create new copy node for 1, traverse to 2, 2 has neighbors 1 and 3
+                    # From 2, we traverse to 1 first and see that it already has a copy node so it hits base case
+                    # Connect copy node 1 to neighbor of 2, traverse to 3 now
+                    # From 3, we traverse back to 2 since its a neighbor and see it already has a copy node so it hits base case
+                    # Connect copy node 2 to neighbor of 3, etc etc
             for neighbor in node.neighbors:
                 copyNode.neighbors.append(dfs(neighbor))
 
@@ -52,9 +58,18 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # Prevents null errors when mapping the first node
         if not node:
             return None
         
@@ -74,6 +89,7 @@ class Solution:
                     queue.append(neighbor) # Append the old node so its own neighbors can be processed later
                 
                 # Map the current vertex's copy to newly copied mapped neighbor
+                    # Essentially creating a new copy connection for the current copied node
                 oldToNew[curr].neighbors.append(oldToNew[neighbor])
         
         # Return the first node since it connects to all other nodes
