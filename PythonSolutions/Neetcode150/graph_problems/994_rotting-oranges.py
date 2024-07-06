77c6cd06f524d1beb454057f6db4b35d649d03d9
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-07-06 01:09:44 -0400

    Added 2nd solution to 994

--- PythonSolutions/Neetcode150/graph_problems/994_rotting-oranges.py
+++ PythonSolutions/Neetcode150/graph_problems/994_rotting-oranges.py
@@ -49,3 +49,40 @@ class Solution:
             minutes += 1
 
         return minutes if fresh == 0 else -1
+    
+class Solution2:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        queue = deque()
+        fresh = 0
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == 2:
+                    queue.append((row, col))
+                elif grid[row][col] == 1:
+                    fresh += 1
+
+        if not fresh:
+            return 0
+        
+        DIRS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        minutes = -1
+
+        while queue:
+            levelSize = len(queue)
+
+            for _ in range(levelSize):
+                row, col = queue.popleft()
+                for dx, dy in DIRS:
+                    newRow, newCol = row + dx, col + dy
+
+                    if 0 <= newRow < ROWS and 0 <= newCol < COLS:
+                        if grid[newRow][newCol] == 1:
+                            grid[newRow][newCol] = 2
+                            queue.append((newRow, newCol))
+                            fresh -= 1
+            
+            minutes += 1
+        
+        return minutes if fresh == 0 else -1
