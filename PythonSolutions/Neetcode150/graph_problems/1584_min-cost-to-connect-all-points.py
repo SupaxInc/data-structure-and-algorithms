8e5596cc5fa3e004f24d3fbb2911c01d81416630
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-07-15 23:30:27 -0400

    Updating 1584 with clarified comments

--- PythonSolutions/Neetcode150/graph_problems/1584_min-cost-to-connect-all-points.py
+++ PythonSolutions/Neetcode150/graph_problems/1584_min-cost-to-connect-all-points.py
@@ -1,21 +1,29 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         res = 0
-        V = len(points)
+        totalVertices = len(points)
         visited = set()
 
         # Beginning at point 0, so our cost is 0
-        pq = [(0, 0)] # (cost, point #)
+        # This can be a random known point on the grid/graph but
+            # starting at 0,0 is a common strategy for Prims algorithm
+        minHeap = [(0, 0)] # (current cost, current point on grid)
 
+        # Manhttan distance is used to calculate the cost of two points
+            # Helps determine the smallest edge to add the to MST in Prims algorithm
         def manhattan(i, j):
             # Manhattan distance: (x1 - x2) + (y1 - y2)
             return abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1])
         
         # Stop Prims algorithm when we have visited total amount of vertices
-            # This will create an MST, since total vertices - 1 = total edges
-        while len(visited) < V:
-            currCost, currPoint = heapq.heappop(pq)
+            # This will create an MST, since totalVertices - 1 = total edges
+        while len(visited) < totalVertices:
+            currCost, currPoint = heapq.heappop(minHeap)
+
             # Undirected graph so there is chance we have already visited a point
+                # This is needed since we are popping from a min heap
+                # That means it will select the minimum cost edge
+                # We may end up popping an edge we have already visited since its randomized based on min cost
             if currPoint in visited:
                 continue
             
@@ -24,8 +32,8 @@ class Solution:
             res += currCost
 
             # Traverse all of the other points since the point can be a neighbour to all points
-            for nextPoint in range(V):
+            for nextPoint in range(totalVertices):
                 if nextPoint not in visited:
-                    heapq.heappush(pq, (manhattan(currPoint, nextPoint), nextPoint))
+                    heapq.heappush(minHeap, (manhattan(currPoint, nextPoint), nextPoint))
         
         return res
