1622313eb0c46edf6cb1f7194b0d9b9ed6bbdc4d
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-07-15 23:54:18 -0400

    Adding kruskal solution to 1584

--- PythonSolutions/Neetcode150/graph_problems/1584_min-cost-to-connect-all-points.py
+++ PythonSolutions/Neetcode150/graph_problems/1584_min-cost-to-connect-all-points.py
@@ -37,3 +37,62 @@ class Solution:
                     heapq.heappush(minHeap, (manhattan(currPoint, nextPoint), nextPoint))
         
         return res
+    
+
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [1] * size
+    
+    def find(self, p):
+        if self.parent[p] != p:
+            self.parent[p] = self.find(self.parent[p])  # Path compression
+        return self.parent[p]
+    
+    def union(self, p, q):
+        rootP = self.find(p)
+        rootQ = self.find(q)
+        if rootP != rootQ:
+            # Union by rank
+            if self.rank[rootP] > self.rank[rootQ]:
+                self.parent[rootQ] = rootP
+            elif self.rank[rootP] < self.rank[rootQ]:
+                self.parent[rootP] = rootQ
+            else:
+                self.parent[rootQ] = rootP
+                self.rank[rootP] += 1
+            return True
+        return False
+
+class UnionFindSolution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        totalVertices = len(points)
+        edges = []
+
+        # Generate all possible edges with their weights (Manhattan distances)
+        for i in range(totalVertices):
+            for j in range(i + 1, totalVertices):
+                weight = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                edges.append((weight, i, j))
+
+        # Sort edges by weight
+            # Allows us to select and connect the most minimum weights first for each point
+        edges.sort()
+
+        # Kruskal's algorithm to form MST
+        uf = UnionFind(totalVertices)
+        mst_cost = 0  # Total cost of the MST
+        edges_used = 0  # Number of edges added to the MST
+
+        for weight, u, v in edges:
+            # Add edges 1 by 1, connecting points (u, v) together to form a graph
+                # Check if it doesn't for a cycle
+            if uf.union(u, v):  
+                mst_cost += weight  # Add the edge's weight to the total cost
+                edges_used += 1  # Increment the count of edges used in the MST
+
+                # If we've used enough edges to form a MST (Edges = Vertices - 1)
+                if edges_used == totalVertices - 1:  
+                    break  # Exit the loop early
+
+        return mst_cost
