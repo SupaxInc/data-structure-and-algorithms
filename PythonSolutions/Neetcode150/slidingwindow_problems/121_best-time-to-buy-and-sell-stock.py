7f720c53d5acdb3a249a37327c20595b600c21d5
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-11-12 22:35:51 -0500

    Updating 121 with better explanation

--- PythonSolutions/Neetcode150/slidingwindow_problems/121_best-time-to-buy-and-sell-stock.py
+++ PythonSolutions/Neetcode150/slidingwindow_problems/121_best-time-to-buy-and-sell-stock.py
@@ -13,7 +13,8 @@ class Solution:
             else:
                 # If the previous day price is greater than the future date price
                 # Then this means we are not able to buy low and sell high
-                # So we update the start to the end
+
+                # So we update the price to the future date price since we found a better buy price as future date is lower
                 start = end
             
             # Move to the next day
