6c800f3fd23a685407d1296c97748825bf6d3f00
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-11-19 23:33:43 -0500

    Added more optimal solution to leetcode 128

--- PythonSolutions/Neetcode150/array_problems/128_longest-consecutive-sequence.py
+++ PythonSolutions/Neetcode150/array_problems/128_longest-consecutive-sequence.py
@@ -1,4 +1,4 @@
-class Solution:
+class SomewhatOptimalSolution:
     def longestConsecutive(self, nums: List[int]) -> int:
         numSet = set(nums)
         longest = 0
@@ -18,3 +18,26 @@ class Solution:
             longest = max(length, longest)
 
         return longest 
+
+class MostOptimalSolution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        maxLength = 0
+
+        # **Iterate through numbers in set instead of list to avoid duplicates**
+        # E.g. [1,2,2,3] -> [1,2,3], only checks 2 once
+        for num in numSet:
+            length = 0
+
+            # Check if there's no left neighbour (start of sequence)
+            if num - 1 not in numSet:
+                length += 1
+
+                # Check if there are further consecutive sequences by checking if the next number sequence is in the set
+                # E.g. 1 + 1 = 2, 1 + 2 = 3, 1 + 3 = 4, etc
+                while num + length in numSet:
+                    length += 1
+                
+                maxLength = max(maxLength, length)
+        
+        return maxLength
