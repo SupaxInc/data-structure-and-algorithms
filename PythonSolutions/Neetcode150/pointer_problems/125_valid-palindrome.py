b8f2feffc2195f317bb82cabaf02a665e6d1b145
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-11-20 23:17:36 -0500

    Updated 125 with better solutions

--- PythonSolutions/Neetcode150/pointer_problems/125_valid-palindrome.py
+++ PythonSolutions/Neetcode150/pointer_problems/125_valid-palindrome.py
@@ -1,4 +1,27 @@
 import re
+class MostOptimalSolution:
+    def isPalindrome(self, s: str) -> bool:
+        # Two pointers: start and end
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            # Skip non-alphanumeric characters from left
+            while left < right and not s[left].isalnum():
+                left += 1
+            
+            # Skip non-alphanumeric characters from right
+            while left < right and not s[right].isalnum():
+                right -= 1
+        
+            # Compare non-alphanumeric characters (case-insensitive) after possibly skipping the characters
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            # Move the pointers inward to check for the next pair of characters in next iteration
+            left += 1
+            right -= 1
+        
+        return True
 
 class Solution1:
     def isPalindrome(self, s: str) -> bool:
@@ -20,15 +43,9 @@ class Solution1:
         
         return True
     
-class Solution2:
+class SomewhatOptimalSolution:
     def isPalindrome(self, s: str) -> bool:
-        newString = ''
-
-        # Create a non-alphanumeric string of the original string
-        for char in s:
-            if char.isdigit() or char.isalpha():
-                newString += char.lower()
-
-        # Compare if the non-alphanumeric string equals to the reverse of it
-        return newString == newString[::-1]
-            
+        # Convert characters into lowercases without alphanumerics
+        chars = [char.lower() for char in s if char.isalnum()]
+        # Check if it equals the reverse of it
+        return chars == chars[::-1]
