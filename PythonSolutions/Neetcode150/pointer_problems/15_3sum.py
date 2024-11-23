07898467dac18d8633baa4b2a69dbca1652b3278
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-11-23 17:53:46 -0500

    updated 15 with better solution

--- PythonSolutions/Neetcode150/pointer_problems/15_3sum.py
+++ PythonSolutions/Neetcode150/pointer_problems/15_3sum.py
@@ -30,40 +30,44 @@ class MySolution:
 class OptimalSolution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-        nums.sort() # Sort the numbers to find duplicate values as neighbors
+        nums.sort()
 
-        # First loop to look for 1st number
-        for i in range(0, len(nums)):
-            # Check the left neighbor of the 1st index for duplicates
+        for i in range(len(nums)-2):
+            # Check duplicates for index 0
+            # If we don't it may result in a duplicate unique triplet result set
+            # E.g. For array: [-1, -1, -1, 2] → [-1, -1, 2], [-1, -1, 2]
             if i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            # If first number is greater than 0, then its impossible to get a sum of 0
+            # Once the first number becomes positive in the array, we can stop as we can't create a total of 0
+            # E.g. [1, 3, 4], impossible to total to 0
             if nums[i] > 0:
                 break
             
-            l, r = i+1, len(nums) - 1
-            # Second loop to look for 2nd and 3rd number
+            l, r = i + 1, len(nums)-1
             while l < r:
                 total = nums[i] + nums[l] + nums[r]
-                # Move pointers depending if the total is greater than or less than the target
+
                 if total < 0:
                     l += 1
                 elif total > 0:
                     r -= 1
                 else:
                     res.append([nums[i], nums[l], nums[r]])
-                    # If the target is currently at 0, we can move the left or right pointers. Doesn't matter which.
                     l += 1
 
-                    # Check for left neighbor duplicates for the 2nd index.
-                    # Loop until it's no longer a duplicate
-                    while nums[l-1] == nums[l] and l < r:
+                    # Check duplicates for index 1
+                    # Loop the next left pointer until we do not find an answer with duplicates
+                    while l < r and nums[l] == nums[l-1]:
                         l += 1
-                    # Do not need to check for neighbor duplicates for 3rd index
-                    # Since we move the right pointer based on if the total is too big
-                    # If we move the right pointer and get the same value then it'll just move it again because total is still big
-                    # This is the benefits of two pointer, the 2nd pointer will have the larger value preventing a 0
-        
+
+                    # No need to check for duplicates for index 2
+                    # 3rd index is taken care of because at this point the left pointer has moved enough
+                    # Which means moving the right pointer to the same value will make the total too large
+                    # So if a duplicate is hit for the 3rd index, it wont matter as the total will still be too large.
+            
         return res
+
+
+
                 
