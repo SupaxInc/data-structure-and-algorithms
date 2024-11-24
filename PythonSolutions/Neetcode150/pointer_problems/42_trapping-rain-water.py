a8e49cbfe1aacc299db023d58d9590bf30964676
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-11-24 17:13:19 -0500

    Updated most optimized solution for 42

--- PythonSolutions/Neetcode150/pointer_problems/42_trapping-rain-water.py
+++ PythonSolutions/Neetcode150/pointer_problems/42_trapping-rain-water.py
@@ -25,6 +25,8 @@ class Solution:
             # Similar to container with most water, we need to get the min of the max heights, so water does not spill over
             # Subtract it by current height to see how much water can be trapped between the two max heights to left and right
             units = min(maxLefts[i], maxRights[i]) - height[i]
+
+            # If units are negative then it means the current height is taller than the max heights to left and right
             if units > 0:
                 total += units
         
@@ -32,27 +34,36 @@ class Solution:
     
 class MostOptimizedSolution:
     def trap(self, height: List[int]) -> int:
-        if len(height) < 2:
+        n = len(height)
+        if n < 2:
             return 0
 
-        l, r = 0, len(height) - 1
-        maxL, maxR = height[l], height[r]
+        maxL, maxR = height[0], height[-1]
         total = 0
 
+        l, r = 0, len(height)-1
+
         while l < r:
-            # The pointer with the lower max height moves toward the other pointer
-                # This is so that the local height (current pointer height) has a higher chance
-                # of trapping water without spilling over.
+            # Find out which heights to left or right of current index is taller
+            # Use the smaller height to calculate with current height
+                # Similar to container most water, use the smaller height so water does not overflow
             if maxL < maxR:
-                l += 1 # Moving pointers right away prevents including calculation from beginning pointers
-                # Update max height seen right away
-                    # This might be unintuitive b/c max height might be current position
-                    # Then the total result would be 0 either way
-                maxL = max(maxL, height[l]) 
-                total += maxL - height[l] # Use left pointer as current height
+                # Move left pointer by 1 instantly since we already have the previous height saved as maxL
+                # This allows us to use l as the current index and calculate it against the max height to left
+                l += 1 # l will now be current index (current height)
+
+                # Get max of current height and the height to left to prevent NEGATIVE units
+                    # If negative, it means water could not fill the current height since the current height is taller
+                maxL = max(maxL, height[l])
+                
+                # Essentially subtracting the highest height to the left to current height
+                    # If the current height is taller from previous max calculation then it will total to just 0
+                    # preventing a negative unit answer.
+                total += maxL - height[l]
             else:
                 r -= 1
+
                 maxR = max(maxR, height[r])
-                total += maxR - height[r] # Use right pointer as current height
+                total += maxR - height[r]
         
         return total
