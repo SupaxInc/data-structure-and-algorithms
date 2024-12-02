71783b2bbbf2cd5bcf30e2dd6d85a5030ea08278
SupaxInc <paulj_gonzales@hotmail.com>
2024-12-01 23:47:23 -0500

    Updated better explanations for leetcode 853

--- PythonSolutions/Neetcode150/stack_problems/853_car-fleet.py
+++ PythonSolutions/Neetcode150/stack_problems/853_car-fleet.py
@@ -1,13 +1,21 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         # Sorted, sorts an iterable list, in this case it is a tuple
-        # The tuple is the zip of position and speed, it combines the two arrays based on the same index and creates a tuple
-        # It will be reversed so its easier to loop through and understand
+        # Zip position and speed, combines the two arrays on the same index and creates a tuple
+        # E.g. position = [10,8,0,5,3], speed = [2,4,1,1,3]
+        # Result: [(10, 2), (8, 4), (5, 1), (3, 3), (0, 1)]
+            # Array is reversed so its more readable
+            # Could loop normally but then we would have to iterate in reverse which is less readable
+        # If we started from left to right, its hard to know if we can join the car in the front since we don’t know if it already joined a fleet thats even more ahead.
         cars = sorted(zip(position, speed), reverse=True)
+
+        # Stores the time, and each time stored symbolizes a fleet
         stack = []
         
         for pos, spd in cars:
+            # Calculate the time it takes for the car to reach the target
             time = (target - pos) / spd
+
             # If the stack is empty then it means we can add one fleet
             # If not empty, compare the current time to the top of the stack (which is the car fleet at the front)
             # If the time is greater, that means the fleet would not likely join the fleet in the front as its too slow
