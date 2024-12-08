5e9e8a72f1ec4b554a5b5acd3de501db14b082a7
SupaxInc <paulj_gonzales@hotmail.com>
2024-12-08 18:00:11 -0500

    updated 704

--- PythonSolutions/Neetcode150/binarysearch_problems/704_binary-search.py
+++ PythonSolutions/Neetcode150/binarysearch_problems/704_binary-search.py
@@ -5,15 +5,16 @@ class Solution:
         lo, hi = 0, len(nums)-1
 
         while lo <= hi:
+            # Check for the new middle point in the array to divide & conquer
             mid = lo + ((hi - lo) // 2) # A normal (hi + lo) // 2 could lead to an arithmetic overflow
 
             if nums[mid] == target:
                 return mid
 
-            # Move to the right side of the array, add a 1 to exclude the current mid index we are on
+            # Move to the right side of the array, add a 1 to exclude the lower boundary
             if nums[mid] < target:
                 lo = mid + 1
-            # Move to the left side of the array, substract a 1 to exclude the current mid index we are on
+            # Move to the left side of the array, substract a 1 to exclude the higher boundary
             elif nums[mid] > target:
                 hi = mid - 1
         
