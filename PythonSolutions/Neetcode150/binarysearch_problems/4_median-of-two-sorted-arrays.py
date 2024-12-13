5a6438209b8602653ac3f2fbc0156ec993d06982
SupaxInc <paulj_gonzales@hotmail.com>
2024-12-12 23:21:42 -0500

    Added better understanding of leetcode 4

--- PythonSolutions/Neetcode150/binarysearch_problems/4_median-of-two-sorted-arrays.py
+++ PythonSolutions/Neetcode150/binarysearch_problems/4_median-of-two-sorted-arrays.py
@@ -1,47 +1,50 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # If nums1 is longer than nums2, swap them to minimize the binary search space
+        # Always make nums1 the shorter array to optimize our binary search
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
             
+        # x is the length of nums1 (shorter array) - used as boundary for binary search
+        # y is the length of nums2 (longer array) - used to calculate partition point in nums2
         x, y = len(nums1), len(nums2)
         low, high = 0, x
         
         while low <= high:
-            # Find the partition point for nums1
-                # **We will be binary searching on partitionX**
+            # Binary search on the smaller array (nums1) to find where to partition it
+            # partitionX represents how many elements we take from nums1 for the left half
             partitionX = (low + high) // 2
-            # To get the partition for nums2, we need the total + 1
-                # +1 allows us to divide by odd or even numbers
+
+            # Once we know how many elements we're taking from nums1,
+            # we can calculate how many we need from nums2 to make the left half complete
+            # The +1 handles both odd and even total lengths
                 # E.g. x = 3, y = 4, 8 // 2 = 4
                 # E.g. x = 4, y = 4, 8 + 1 = 9 // 2 = 4.5 = 4
             partitionY = (x + y + 1) // 2 - partitionX
 
-            # Elements immediately left of the partition in nums1
-            # If partitionX is 0, use negative infinity as there are no elements left
+            # Get the elements around the partition point
+            # For nums1: maxX is the last element on left side, minX is first element on right side
             maxX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
-            # Elements immediately left of the partition in nums2
-            maxY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
-            
-            # Elements immediately right of the partition in nums1
-            # If partitionX is at the end of nums1, use positive infinity as there are no elements right
             minX = float('inf') if partitionX == x else nums1[partitionX]
-            # Elements immediately right of the partition in nums2
+            
+            # For nums2: maxY is the last element on left side, minY is first element on right side
+            maxY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
             minY = float('inf') if partitionY == y else nums2[partitionY]
 
-            # Check if we have found the correct partition
-                # Ensure that all elements on the left are less than or equal to those on the right across both arrays
-                # If its not valid an example would be: [... 4] [3...], our right partition cannot be greater than left
+            # Valid partition means:
+            # 1. All elements in left half ≤ all elements in right half
+            # 2. maxX ≤ minY and maxY ≤ minX
             if maxX <= minY and maxY <= minX:
-                # If the total number of elements is even, return the average of the two middle elements
+                # For even total length: median is average of max of left half and min of right half
                 if (x + y) % 2 == 0:
                     return (max(maxX, maxY) + min(minX, minY)) / 2
+                # For odd total length: median is the max element in the left half
                 else:
-                    # If the total number of elements is odd, return the maximum of the two elements left of the partition
                     return max(maxX, maxY)
+                    
+            # If partition is wrong, adjust binary search:
             elif maxX > minY:
-                # If elements on the left of nums1 are too big, move the partition left
+                # Left side of nums1 is too big, need to take fewer elements from nums1
                 high = partitionX - 1
             else:
-                # If elements on the left of nums1 are too small, move the partition right
+                # Left side of nums1 is too small, need to take more elements from nums1
                 low = partitionX + 1
