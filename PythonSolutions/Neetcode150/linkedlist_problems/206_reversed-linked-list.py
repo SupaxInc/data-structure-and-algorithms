e2472459482f4d7948ba0df771c3ab92bc333deb
SupaxInc <paulj_gonzales@hotmail.com>
2024-12-15 21:18:30 -0500

    updated 206

--- PythonSolutions/Neetcode150/linkedlist_problems/206_reversed-linked-list.py
+++ PythonSolutions/Neetcode150/linkedlist_problems/206_reversed-linked-list.py
@@ -8,18 +8,18 @@ class Solution:
         if not head:
             return head
         
-        currNode, prevNode = head, None
+        cur, prv = head, None
 
-        while currNode:
+        while cur:
             # Create a temp pointer to the next node of the current node
-            temp = currNode.next
+            prv = cur.next
             # The next node of the current node will point to the previous node e.g 1 -> 2 is now 1 -> None (pointing to new end)
-            currNode.next = prevNode
+            cur.next = prv
             # The previous node becomes the current node (the current node points to the previous node correctly making a reversal)
-            prevNode = currNode
+            prv = cur
             # The current node is able to grab the reference to the next node which wasn't reversed
-            currNode = temp
+            cur = prv
 
         # Return the previous node as that is now the new head as it references the curr node last
             # The curr node points to temp which is the null end of the linked list
-        return prevNode
+        return prv
