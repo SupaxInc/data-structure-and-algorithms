82ed9e577ef39aa00f5fdd11f361d48c197df3a5
SupaxInc <paulj_gonzales@hotmail.com>
2024-12-27 17:27:57 -0500

    updating 19

--- PythonSolutions/Neetcode150/linkedlist_problems/19_remove-nth-node-from-list.py
+++ PythonSolutions/Neetcode150/linkedlist_problems/19_remove-nth-node-from-list.py
@@ -5,9 +5,11 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # * Edge case: * 
         # Without a dummy node, we wont have a pointer of the previous node from the target
         # Our slow pointer would end up on the target and we wont be able to remove target node
         dummy = ListNode(0, head)
+
         slow = dummy # Slow pointer starts at dummy node, this is so we don't end up on the target node
         fast = head # Fast pointer starts at head
 
@@ -15,18 +17,19 @@ class Solution:
         for _ in range(0, n):
             fast = fast.next
         
-        # Move both pointers just once until we find the end
+        # Move both pointers 1 step at a time until fast pointer hits end
         while fast:
             slow = slow.next
             fast = fast.next
 
-        # Since we're now at the previous node of the target
+        # Slow is now at the previous node of the target since it started at the dummy node
         # We can point the next of the previous node to the node in front of target node
-        # This effectively remvoes the target node
+        # This effectively removes the target node
         slow.next = slow.next.next
 
         # Return the next of the dummy to get the list from original head
             # This is because from the start we referenced slow to the dummy node
-            # So when we removed the target node from slow, dummy still references slow list
+            # Since we removed the target node from slow pointer, dummy will reference the slow list
         return dummy.next
+        #* Returning head here will be wrong since we don't use the fast pointer to remove the target node but use the slow pointer *
         
