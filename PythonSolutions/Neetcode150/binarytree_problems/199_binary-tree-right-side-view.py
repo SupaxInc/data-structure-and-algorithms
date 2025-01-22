c52f35cc51d8f52ba72a2f7b1a502b268c5dcd49
SupaxInc <paulj_gonzales@hotmail.com>
2025-01-21 23:41:26 -0500

    updated 199

--- PythonSolutions/Neetcode150/binarytree_problems/199_binary-tree-right-side-view.py
+++ PythonSolutions/Neetcode150/binarytree_problems/199_binary-tree-right-side-view.py
@@ -13,13 +13,16 @@ class Solution:
         queue = deque([root])
 
         while queue:
+            # Queue length should be outside of the for loop so we don't recalculate the size of the level per iteration in the loop
             queueLength = len(queue)
+
             # The for loop will only check the the nodes for the current level
                 # This is because queue length stays the same and does not change within the for loop
                 # It gets recalculate when for loop is done
             for i in range(queueLength):
                 curr = queue.popleft()
-                # If its at the last node in the level, add to result
+
+                # Only add last node of the level so its only grabbing the far right node from view
                 if i == queueLength - 1:
                     res.append(curr.val)
     
