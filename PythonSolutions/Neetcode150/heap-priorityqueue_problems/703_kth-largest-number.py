2b634fe61a7c94e59c1a9e4ae09a5469fb73e2a3
SupaxInc <paulj_gonzales@hotmail.com>
2025-02-04 23:29:46 -0500

    updated 703 to have no prints and better explanation

--- PythonSolutions/Neetcode150/heap-priorityqueue_problems/703_kth-largest-number.py
+++ PythonSolutions/Neetcode150/heap-priorityqueue_problems/703_kth-largest-number.py
@@ -1,30 +1,26 @@
 import heapq
+
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.minHeap, self.k = nums, k
+        self.k, self.minHeap = k, nums
         heapq.heapify(self.minHeap)
 
-        # We want the heap to only have kth size
-        # This allows us to return the kth largest element at the top of the min heap
-            # Since its a min heap, the values that are left after popping are the largest
-            # Therefore, the next pop will be the kth largest
-        while len(self.minHeap) > self.k:
-            print(heapq.heappop(self.minHeap))
-        
-        print(self.minHeap)
+        # Make the min heap size of Kth largest so its easier to know what the kth largest is
+        while(len(self.minHeap) > self.k):
+            heapq.heappop(self.minHeap)
 
     def add(self, val: int) -> int:
-        # Add the value to the heap
+        # Add the new value to the min heap
         heapq.heappush(self.minHeap, val)
 
-        # Edge case, need to pop heap again once since its now greater than kth size after pushing new value
+        # Now that the min heap is larger than the length of Kth largest
+        # Pop it so that we keep track of what the Kth largest is
         if len(self.minHeap) > self.k:
             heapq.heappop(self.minHeap)
         
-        # The top of the heap is always accessed by index 0
+        # Since the min heap is again the size of kth largest, return the top of the heap (smallest value in the heap)
         return self.minHeap[0]
-        
 
 
 # Your KthLargest object will be instantiated and called as such:
