12db069aae60b6b80e021a574a3bb352b0ee7185
SupaxInc <paulj_gonzales@hotmail.com>
2025-02-12 21:47:58 -0500

    updated 78

--- PythonSolutions/Neetcode150/backtracking_problems/78_subsets.py
+++ PythonSolutions/Neetcode150/backtracking_problems/78_subsets.py
@@ -1,27 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        res = []
         subset = []
 
         def backtrack(start):
             # No need for a base case, for loop takes care of only constraint
                 # Which is to stop recursing when we reach the length of nums
+            res.append(subset[:])
 
-            # Append subset right away to include empty subsets
-                # Also adds a new subset to result per recursion
-            result.append(subset[:]) # Deep copy
-
-            # Iterate through all choices we have
             for i in range(start, len(nums)):
-                # Append the choice (inclusion)
+                # Include the choice
                 subset.append(nums[i])
-                
-                # Explore the choice deeper
-                backtrack(i + 1)
 
-                # Undo the choice (exclusion)
+                # Recurse, explore the next option deeper
+                backtrack(i+1)
+
+                # Exclude, backtrack to previous choice
                 subset.pop()
-
-        # Begin at the first choice
+        
         backtrack(0)
-        return result
+        return res
