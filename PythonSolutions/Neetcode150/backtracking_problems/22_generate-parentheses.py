02799eaee1c8ce8724eb1022555e6bab8e4d2182
SupaxInc <paulj_gonzales@hotmail.com>
2025-02-13 21:50:49 -0500

    updated leetcode 22 answer

--- PythonSolutions/Neetcode150/backtracking_problems/22_generate-parentheses.py
+++ PythonSolutions/Neetcode150/backtracking_problems/22_generate-parentheses.py
@@ -1,21 +1,29 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        def backtrack(open_n, closed_n, path):
-            # Append path if the amount of open and closed parentheses are equal
-            if closed_n == open_n == n:
+
+        def backtrack(open_b, close_b, path):
+            """
+                Args:
+                    open_b: # of open brackets
+                    close_b: # of closing brackets
+                    path: current parentheses combinations
+            """
+            # Success found: Open and closed brackets create the correct amount of nth pairs
+            if open_b == close_b == n:
                 res.append(path)
-                # Prune search space when we find a path that is valid
+                # Prune the search space when path found
+                # Its impossible to find more paths when we already have the most amount of nth brackets
                 return
             
-            # Inclusion choice: Add an open bracket 
-            if open_n < n:
-                backtrack(open_n + 1, closed_n, path + "(")
-            
-            # Exclusion choice: Don't add an open bracket
-                # Add closing brackets instead when there are less amounts of open brackets
-            if closed_n < open_n:
-                backtrack(open_n, closed_n + 1, path + ")")
-        
+            # * The first choice is always adding an open bracket to prevent false parentheses combinations * 
+            # Inclusion choice 1: Add an open bracket if we haven't used all n open brackets
+            if open_b < n:
+                backtrack(open_b + 1, close_b, path + "(")
+
+            # Inclusion choice 2: Add a closing bracket if we have more open than closed brackets
+            if close_b < open_b:
+                backtrack(open_b, close_b + 1, path + ")")
+    
         backtrack(0, 0, "")
         return res
