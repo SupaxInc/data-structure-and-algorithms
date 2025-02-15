bee2a2504afaa87bd9a2b6ca2531874921b5a6ee
SupaxInc <paulj_gonzales@hotmail.com>
2025-02-15 00:21:37 -0500

    updating 46 with better explanation comments

--- PythonSolutions/Neetcode150/backtracking_problems/46_permutations.py
+++ PythonSolutions/Neetcode150/backtracking_problems/46_permutations.py
@@ -17,7 +17,11 @@ class Solution:
                     # Changes the range of the for loop from start + 1 to len(nums)
                     # E.g. [1, 2, 3]
                     # Initial -> Idx: 0 to 3 [1, 2, 3] -> Explore -> 1 to 3 [2, 3] -> 
-                    # Explore -> 2 to 3 [3] -> Explore -> 3 to 3 [] -> Hits Base Case 
+                    # Explore -> 2 to 3 [3] -> Explore -> 3 to 3 [] -> Hits Base Case
+                # We are using start here instead of index i because think of it like:
+                    # start = "Which positions do I want to look for all permutations for next?"
+                    # i = "Which number do I want to test for permutations?"
+                    # Start makes more sense since we want to try permutations for all the next numbers over rather than just selecting a specific number
                 backtrack(start + 1)
 
                 # Undo swap
