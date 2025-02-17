e7fd32409af8f94ce9ecb3f61144c1f32d053127
SupaxInc <paulj_gonzales@hotmail.com>
2025-02-17 15:26:00 -0500

    added more efficient solution to 17

--- PythonSolutions/Neetcode150/backtracking_problems/17_letter-combinations-of-a-phone-number.py
+++ PythonSolutions/Neetcode150/backtracking_problems/17_letter-combinations-of-a-phone-number.py
@@ -1,3 +1,38 @@
+class MoreEfficientSolution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        if not digits: 
+            return res
+        
+        # Create a digit map for the telephone buttons
+        digitMap = { "2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz" }
+        path = []
+
+        def backtrack(digitIdx):
+            # Base case: Path is the same length as digits
+            if len(path) == len(digits):
+                # Turn path into a string
+                res.append("".join(path)) 
+                # Prune the path since there are no more combinations to look for
+                return
+            
+            # Grab the letters for the digit of the phone number
+            letters = digitMap[digits[digitIdx]]
+
+            for letter in letters:
+                # Inclusion choice: Add the letter
+                path.append(letter)
+
+                # Explore the current path (current letters) as deep as possible until we hit the digits length
+                backtrack(digitIdx + 1)
+
+                # Exclusion choice: Remove the letter and try other options
+                path.pop()
+
+        backtrack(0)
+        return res
+
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         res = []
