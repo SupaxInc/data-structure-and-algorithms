16096d37d36f4ff309296bc1a8bb3a9fa3252992
SupaxInc <paulj_gonzales@hotmail.com>
2025-02-18 23:47:51 -0500

    adding better dfs solution to 200

--- PythonSolutions/Neetcode150/graph_problems/200_number-of-islands.py
+++ PythonSolutions/Neetcode150/graph_problems/200_number-of-islands.py
@@ -1,33 +1,38 @@
-class DFSSolution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+class DFSSolution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        self.grid = grid
+        self.ROWS, self.COLS = len(grid), len(grid[0])
+        self.DIRS = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
         count = 0
 
-        for row in range(0, len(grid)):
-            for col in range(0, len(grid[0])):
-                # "Sink" the island so that its easier to count the islands
-                if grid[row][col] == "1":
-                    self.dfs(grid, row, col)
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if self.grid[row][col] == "1":
+                    # Run a DFS once an island has been found, this will do a DFS on the entire island
+                    # This "sinks" the entire island, so next loop iteration will not find anymore "1"s
+                    self.dfs(row, col)
+
+                    # Count this as 1 island after sinking the entire island
                     count += 1
         
         return count
-    
-    def dfs(self, grid, row, col):
-        # Check if the cell were on is valid
-        if row < 0 or col < 0 or row > len(grid)-1 or col > len(grid[0])-1 or grid[row][col] == "0":
+
+    def dfs(self, row, col):
+        # Base case 1: Boundary check
+        if row > self.ROWS-1 or col > self.COLS-1 or row < 0 or col < 0:
             return
         
-        # Visit the cell
-        grid[row][col] = "0"
+        # Base case 2: Visited check
+        if self.grid[row][col] == "0":
+            return
+        
+        # Visit the cell, "sink" a part of the island
+        self.grid[row][col] = "0"
 
-        # Traverse the cell in four directions
-        self.dfs(grid, row + 1, col)
-        self.dfs(grid, row, col + 1)
-        self.dfs(grid, row - 1, col)
-        self.dfs(grid, row, col - 1)
+        for dx, dy in self.DIRS:
+            # Continue to sink all of the islands that have not been visited yet
+            self.dfs(row+dx, col+dy)
 
 class BFSSolution(object):
     def numIslands(self, grid):
