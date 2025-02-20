975c75918d76a8f0bc477128f0a2f16e3eb5c01a
SupaxInc <paulj_gonzales@hotmail.com>
2025-02-20 00:51:34 -0500

    added better DFS solution to 695

--- PythonSolutions/Neetcode150/graph_problems/695_max-area-of-island.py
+++ PythonSolutions/Neetcode150/graph_problems/695_max-area-of-island.py
@@ -1,29 +1,39 @@
 class DFSSolution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        self.grid = grid
+        self.ROWS, self.COLS = len(grid), len(grid[0])
+        self.DIRS = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
         maxArea = 0
 
-        for row in range(0, len(grid)):
-            for col in range(0, len(grid[0])):
-                if grid[row][col] == 1:
-                    area = self.dfs(grid, row, col, 0)
-                    maxArea = max(maxArea, area)
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if self.grid[row][col] == 1:
+                    maxArea = max(maxArea, self.dfs(row, col))
         
         return maxArea
 
-    def dfs(self, grid, row, col, area):
-        if row < 0 or col < 0 or row > len(grid) - 1 or col > len(grid[row]) - 1:
-            return 0
-        if grid[row][col] == 0:
+    def dfs(self, row, col):
+        # Base case 1: Boundary check
+        if row > self.ROWS-1 or col > self.COLS-1 or row < 0 or col < 0:
+            # Returns 0 as its not a valid area
             return 0
         
-        grid[row][col] = 0
-        area = 1
-        area += self.dfs(grid, row + 1, col, area)
-        area += self.dfs(grid, row, col + 1, area)
-        area += self.dfs(grid, row - 1, col, area)
-        area += self.dfs(grid, row, col - 1, area)
+        # Base case 2: Visited check
+        if self.grid[row][col] == 0:
+            # Returns 0 as its not a valid area
+            return 0
+        
+        # Visit the cell
+        self.grid[row][col] = 0
+        totalArea = 1 # The total area for current cell is just a 1
 
-        return area
+        for dx, dy in self.DIRS:
+            # Explore the four directions and add the total area of them
+            totalArea += self.dfs(row+dx, col+dy)
+        
+        # Finally, return the last summed total area of all traveled cells
+        return totalArea
         
 class BFSSolution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
