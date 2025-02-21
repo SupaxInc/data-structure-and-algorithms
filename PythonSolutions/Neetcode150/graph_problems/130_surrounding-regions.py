31b8232aea1044e31aace081b98b4594931e3ffd
SupaxInc <paulj_gonzales@hotmail.com>
2025-02-20 23:51:49 -0500

    updated better solution to 130

--- PythonSolutions/Neetcode150/graph_problems/130_surrounding-regions.py
+++ PythonSolutions/Neetcode150/graph_problems/130_surrounding-regions.py
@@ -3,50 +3,47 @@ class CleanerSolution:
         """
         Do not return anything, modify board in-place instead.
         """
-        self.ROWS, self.COLS = len(board), len(board[0])
         self.board = board
+        self.ROWS, self.COLS = len(board), len(board[0])
         self.DIRS = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
-        # Capture regions that ARE NOT surrounded
-            # Regions that are on the edges of the board can never be surrounded, so we need to capture it temporarily
-            # Makes it easier to capture surrounded regions later since they are no longer "O"'s
+        # 1) Convert O's in the boundary as temp
         for row in range(self.ROWS):
             for col in range(self.COLS):
-                if self.board[row][col] == "O" and (row in [0, self.ROWS - 1] or col in [0, self.COLS - 1]):
-                    # Do DFS on the edges since if a region is connected to a region on the edge
-                    # It could no longer be captured as well
+                # - Check if row is in top or bottom, and if col is in left or right boundaries
+                # - Check if the region is an "O" cell
+                if (row in [0, self.ROWS-1] or col in [0, self.COLS-1]) and self.board[row][col] == "O":
+                    # Run the DFS on the "O" in the boundary 
+                    # since ANY region touching this "O" will no longer able to be surrounded by "X"'s
                     self.dfs(row, col)
-
-        # Capture regions that ARE surrounded
-            # We are able to just capture ALL regions now since they would all be surrounded
-            # As we've captured all regions that can't be surrounded
+        
+        # 2) Convert O's not in the boundary as X's
         for row in range(self.ROWS):
             for col in range(self.COLS):
-                if self.board[row][col] == "O": # Regions that aren't surrounded are "TEMP"s
+                if self.board[row][col] == "O":
                     self.board[row][col] = "X"
         
-        # Remove the capture on regions that ARE NOT surrounded
-            # This is needed so that our board has the correct information of regions that are not surrounded
+        # 3) Convert temps back to O's
         for row in range(self.ROWS):
             for col in range(self.COLS):
                 if self.board[row][col] == "TEMP":
                     self.board[row][col] = "O"
-        
+    
     def dfs(self, row, col):
-        # Boundary check
-        if row > self.ROWS - 1 or col > self.COLS - 1 or row < 0 or col < 0:
+        # Base case 1: Boundary check
+        if row > self.ROWS-1 or col > self.COLS-1 or row < 0 or col < 0:
             return
         
-        # Valid check, if its no longer a region (could be temp or an X)
+        # Base case 2: Visited check and if its not an "O" don't change it to TEMP 
         if self.board[row][col] != "O":
             return
         
-        # Convert regions to a temp so it could no longer be captured later
+        # Visit the cell
         self.board[row][col] = "TEMP"
 
         for dx, dy in self.DIRS:
             self.dfs(row+dx, col+dy)
-        
+
         return
 
 class Solution:
