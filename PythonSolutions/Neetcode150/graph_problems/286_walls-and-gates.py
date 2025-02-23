2c761eea11942f100865605479336ce2621d4e2a
SupaxInc <paulj_gonzales@hotmail.com>
2025-02-22 23:16:21 -0500

    updated 286

--- PythonSolutions/Neetcode150/graph_problems/286_walls-and-gates.py
+++ PythonSolutions/Neetcode150/graph_problems/286_walls-and-gates.py
@@ -1,34 +1,50 @@
 class Solution:
+    # *Don't have premium leetcode so this is using Islands and Treasure from Neetcode: https://neetcode.io/problems/islands-and-treasure*
     def islandsAndTreasure(self, grid: List[List[int]]) -> None:
-        ROWS, COLS = len(grid), len(grid[0])
-        INF = 2**31 - 1
-        queue = deque([])
+        self.grid = grid
+        self.ROWS, self.COLS = len(grid), len(grid[0])
+        self.DIRS = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
-        # Traverse the grid and create the necesary INF land values along with adding treasure chests to queue
-        for row in range(ROWS):
-            for col in range(COLS):
-                if grid[row][col] == 0:
+        self.INF = 2**31 - 1
+        queue = deque()
+
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                # Add each treasure chest to a queue so we can look for distance at the same time
+                if self.grid[row][col] == 0:
                     queue.append((row, col))
-                elif grid[row][col] != -1:
-                    grid[row][col] = INF
+                # Fill the land cells as INF since there may be land that can't be traversed
+                elif self.grid[row][col] != -1:
+                    self.grid[row][col] = self.INF
         
-        DIRS = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-        dist = 0
-
-        # Level order traversal, begin at the treasure chests at the same time
+        self.levelOrderBFS(queue)
+        
+    def levelOrderBFS(self, queue):
+        distance = 0
+        
         while queue:
             levelSize = len(queue)
-            dist += 1
-            
+            # Increment the distance early since we don't count current level
+            distance += 1
+
             for _ in range(levelSize):
                 row, col = queue.popleft()
 
-                for dx, dy in DIRS:
-                    newRow, newCol = row + dx, col + dy
+                # Check for cells in the NEXT level and not the current level
+                # This is because we are counting the distance from the current level
+                for dx, dy in self.DIRS:
+                    newRow, newCol = row+dx, col+dy
 
-                    # Check if the new traversed grid is valid
-                    if 0 <= newRow < ROWS and 0 <= newCol < COLS:
-                        # If its a land value add the distance then append the grid so it can be traversed next level
-                        if grid[newRow][newCol] == INF:
-                            grid[newRow][newCol] = dist
-                            queue.append((newRow, newCol))
+                    # Base case 1: Boundary check
+                    if newRow > self.ROWS-1 or newCol > self.COLS-1 or newRow < 0 or newCol < 0:
+                        continue
+                    
+                    # Base case 2: Not a land cell so we can't traverse
+                    if self.grid[newRow][newCol] != self.INF:
+                        continue
+
+                    # Visit the cell and add the distance
+                    self.grid[newRow][newCol] = distance
+
+                    queue.append((newRow, newCol))
+                
