457effd45bed7e7dd5e392a2aeb66261174308f1
SupaxInc <paulj_gonzales@hotmail.com>
2025-03-02 16:22:50 -0500

    updated the union find solution to 684

--- PythonSolutions/Neetcode150/graph_problems/684_redundant-connections.py
+++ PythonSolutions/Neetcode150/graph_problems/684_redundant-connections.py
@@ -1,49 +1,48 @@
 class UnionFind:
     def __init__(self):
-        # Since theres no size, we use a hashmap
-        self.parent = {}
+        # Size is not given so create an empty dict
+        self.root = {}
         self.rank = {}
     
     def find(self, x):
-        if x not in self.parent:
-            self.parent[x] = x
+        # If the node does not exist yet in the tree, add it
+        if x not in self.root:
+            self.root[x] = x
             self.rank[x] = 1
-        if x != self.parent[x]:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+        
+        # Check if the node is a root of itsself, if not flatten the structure of the tree
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
+        
+        return self.root[x]
     
     def union(self, x, y):
-        # This will continuously create a union between two nodes
-        # At some point we end up with a node already in a group
-        # Where cycle gets detected below
-        parentX = self.find(x)
-        parentY = self.find(y)
+        rootX = self.find(x)
+        rootY = self.find(y)
 
-        # Cycle has been detected
-            # When parent nodes equal each other it means the edge that we will connect will form a cycle
-            # Due to nodes pointing to parent nodes to show which group the connection it is in
-            # If they are in the same group, it means the edge we are connecting will form its own connection
-            # Thus creating a loop
-        if parentX == parentY:
-            return False
-        else:
-            if self.rank[parentX] > self.rank[parentY]:
-                self.parent[parentY] = parentX
-            elif self.rank[parentX] < self.rank[parentY]:
-                self.parent[parentX] = parentY
-            else:
-                self.parent[parentY] = parentX
-                self.rank[parentX] += 1
+        # If root X and and root Y are the same, then that means the two trees exist in the same component
+            # This means a cycle will be created if we union both x and y
+        if rootX == rootY:
+            return True
         
-        return True
+        if self.rank[rootX] > self.rank[rootY]:
+            self.root[rootY] = rootX
+        elif self.rank[rootY] > self.rank[rootX]:
+            self.root[rootX] = rootY
+        else:
+            self.root[rootX] = rootY
+            self.rank[rootY] = rootX
+        
+        # Return false since no cycle will be created at this point
+        return False
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        uf = UnionFind() # No size of graph given
+        uf = UnionFind()
 
-        for x, y in edges:
-            if not uf.union(x,y):
-                return [x,y]
+        for u, v in edges:
+            if uf.union(u, v):
+                return [u, v]
         
         return []
 
