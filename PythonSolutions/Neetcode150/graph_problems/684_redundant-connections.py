9d66d996bb71b4c2060e1314a344cb7155c77620
SupaxInc <paulj_gonzales@hotmail.com>
2025-03-02 16:03:19 -0500

    add dfs solution to 684

--- PythonSolutions/Neetcode150/graph_problems/684_redundant-connections.py
+++ PythonSolutions/Neetcode150/graph_problems/684_redundant-connections.py
@@ -46,3 +46,43 @@ class Solution:
                 return [x,y]
         
         return []
+
+class DFSSolution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        self.graph = defaultdict(list)
+
+        for u, v in edges:
+            # Create a new set every DFS call to check if there is a path between source and target
+            visited = set()
+
+            # Check:
+                # - If u and v are in the graph before calling a DFS between the two nodes
+                # - Prevents DFS since if these nodes don't exist then we can't find a path from source to target
+            if u in self.graph and v in self.graph and self.dfs(u, v, visited):
+                # Return the edge that creates a cycle
+                return [u, v]
+            
+            # Connect the edges to the graph unidirectionally
+            self.graph[u].append(v)
+            self.graph[v].append(u)
+        
+        return []
+    
+    def dfs(self, source, target, visited):
+        # Base case 1: Check if we end up finding an existing path to target already
+        if source == target:
+            return True
+        
+        # Base case 2: Check if we have already visited the node
+        if source in visited:
+            return False
+        
+        visited.add(source)
+
+        # Explore all neighbors until we possibly find the target node
+        for nei in self.graph[source]:
+            if self.dfs(nei, target, visited):
+                return True
+        
+        # Return False since we could not find a valid path to target
+        return False
