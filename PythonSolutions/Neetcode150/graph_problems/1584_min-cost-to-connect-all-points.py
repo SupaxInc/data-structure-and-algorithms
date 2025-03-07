e5933c9c19430e08c79c9a043396d3a67d188ac0
SupaxInc <paulj_gonzales@hotmail.com>
2025-03-07 01:11:28 -0500

    adding better solution to 1584 for prim

--- PythonSolutions/Neetcode150/graph_problems/1584_min-cost-to-connect-all-points.py
+++ PythonSolutions/Neetcode150/graph_problems/1584_min-cost-to-connect-all-points.py
@@ -1,42 +1,45 @@
-class Solution:
+class PrimSolution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        res = 0
+        # *This problem is a COMPLETE graph*
+            # Meaning that all points can connect with each other
+            # Therefore, we are not limited only to the vertices that we have visited
+            # We can actually visit any vertex that we want
         totalVertices = len(points)
         visited = set()
+        cost = 0
 
-        # Beginning at point 0, so our cost is 0
-        # This can be a random known point on the grid/graph but
-            # starting at 0,0 is a common strategy for Prims algorithm
-        minHeap = [(0, 0)] # (current cost, current point on grid)
+        # Typically in prim's algorithm, you want to start at point 0 but this can be a random point
+        minHeap = [(0, 0)] # (current cost, current point)
 
-        # Manhattan distance is used to calculate the cost of two points
-            # Helps determine the smallest edge to add the to MST in Prims algorithm
+        # Calculate the manhattan distance between two points (x1 - x2) + (y1 - y2)
+        # This will be used as the cost of the points
         def manhattan(i, j):
-            # Manhattan distance: (x1 - x2) + (y1 - y2)
-            return abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1])
-        
-        # Stop Prims algorithm when we have visited total amount of vertices
-            # This will create an MST, since totalVertices - 1 = total edges
+            return abs(points[i][0] - points[j][0]) + abs(points[i][1]-points[j][1])
+
+        # Stop the BFS when we hit E = V - 1
         while len(visited) < totalVertices:
+            # Get the lowest cost point in heap
             currCost, currPoint = heapq.heappop(minHeap)
 
-            # Undirected graph so there is chance we have already visited a point
-                # This is needed since we are popping from a min heap
-                # That means it will select the minimum cost edge
-                # We may end up popping an edge we have already visited since its randomized based on min cost
+            # Even though we only explore vertices that have not been visited (see exploration below)
+            # There is still a chance that a vertex has been visited since we are randomly popping from a min heap
             if currPoint in visited:
                 continue
-            
-            # Visit the vertex
-            visited.add(currPoint)
-            res += currCost
 
-            # Traverse all of the other points since the point can be a neighbour to all points
-            for nextPoint in range(totalVertices):
-                if nextPoint not in visited:
-                    heapq.heappush(minHeap, (manhattan(currPoint, nextPoint), nextPoint))
-        
-        return res
+            # Visit the vertex
+            cost += currCost
+            visited.add(currPoint)
+
+            # Explore ALL the vertices and calculate ALL of the cost for each vertices
+                # It is a COMPLETE graph we can visit any vertex
+                # neiPoint is an index in the points list which matches to a vertex
+                # e.g. points = [[0,0],[2,2]], neiPoint = 1 is vertex 1 which is point [2, 2]
+            for neiPoint in range(totalVertices):
+                # Only explore the and calculate the cost of the points that have not been visited
+                if neiPoint not in visited:
+                    heapq.heappush(minHeap, (manhattan(currPoint, neiPoint), neiPoint))
+
+        return cost
     
 
 class UnionFind:
