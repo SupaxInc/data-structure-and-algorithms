1fed864abf60f7e8d70dbdc3a67ac510fd079a86
SupaxInc <paulj_gonzales@hotmail.com>
2025-03-23 01:21:19 -0400

    updated bottom up solution for 322 with way better intuition and explanations

--- PythonSolutions/Neetcode150/dynamic-programming_problems/322_coin-change.py
+++ PythonSolutions/Neetcode150/dynamic-programming_problems/322_coin-change.py
@@ -1,26 +1,54 @@
+"""
+- coins array length from 1 to 12
+- amount int from 1 to 10^4
+- coins[i] consists of 32 bit numbers 
+    - assume we have an infinite number of coins[i] that we can use
+- RETURN: Fewest number of coins[i]
+
+State: dp[i], i is the amount at that point, and dp[i] represents the amount of coins used at that amount
+    - NOTE: dp[i] is the AMOUNT of coins used to reach amount, not the EXACT coins that were used
+
+Recurrence Relation: At each amount i, find the number of coins used to reach the amount i
+Transition: min(dp[i], dp[i-coin] + 1)
+    - Select the min between:
+        - dp[i], represents best solution found so far using other coins with the SAME amount
+        - dp[i-coin] + 1, represents best solution for the remaining amount after USING the coin (adding +1 uses the coin)
+            * SIMPLIFIED: After using the coin at the current amount, what was the best PREVIOUS solution for the remaining amount? *
+
+Base cases:
+1. dp[0] = 0, for an amount of 0, we USE 0 coins
+ 
+Returns: -1 if its not possible, else dp[amount] which is the target amount
+"""
 class BottomUpSolution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Amount + 1, since its an upper boundary limit
-            # We will never need more coins than the amount + 1
-            # Helps figure out if we went over the amount
-        dp = [amount + 1] * (amount + 1)
-
-        # Base case: Takes 0 coins to get amount of 0
-        dp[0] = 0 
-
-        for i in range(1, amount + 1):
-            for coin in coins:
-                # If the current coin can contribute to the current amount i
-                    # then we check if using current coin would reduce the number of coins needed.
-                # Essentially prunes the search for the coin if it becomes negative
-                if i - coin >= 0:
-                    # Update to min of its current value
-                        # Or the number of coins needed for (i - coin) plus one (for the current coin)
-                        # E.g. i = 4, coin = 2, i-coin = 2, dp[2] + 1 (1 more coin 2) = 1 + 1 = 2 
-                    dp[i] = min(dp[i], dp[i-coin] + 1)
+        # Initialize DP state: coinsUsed[i], represents the amount of coins used at amount i
+            # Value is infinity since its impossible to actually reach that value
+            # Length is amount+1 so that we can account for the base case
+        coinsUsed = [float("inf")] * (amount+1)
         
-        # Return latest cached amount if it changed, else - 1
-        return dp[amount] if dp[amount] != amount + 1 else -1
+        # Base case: For an amount of 0, use 0 coins
+        coinsUsed[0] = 0
+
+        # Recurrence relation: At each amount i, find number of coins used to reach the amount
+        for i in range(1, amount+1):
+
+            # Check the amount after trying each coin
+            for coin in coins:
+                amountAfterUsingCoin = i-coin
+
+                # Check if the current coin can contribute to the amount
+                    # If positive number, was able to contribute 
+                    # If negative number, coin goes over limit of amount
+                if amountAfterUsingCoin >= 0:
+                    # Transition: Check min between current amount of remaining amount
+                        # coinsUsed[i], represents best solution found so far SAME amount after trying other coins
+                        # coinsUsed[i-coin], represents best solution from PREVIOUS remaining amount after using coin
+                        # +1, is using the current coin to add to the amount of coins used
+                    coinsUsed[i] = min(coinsUsed[i], coinsUsed[amountAfterUsingCoin] + 1)
+        
+        # Return the best possibilities for target amount if it was possible
+        return coinsUsed[amount] if coinsUsed[amount] != float("inf") else -1
 
 class TopDownSolution:
     def coinChange(coins, amount):
@@ -76,6 +104,9 @@ class BruteForceDFS:
             
             # Try using each coin
             for coin in coins:
+                # Inclusion: Subtracting the coin from remainder and adding the coin to the count
+                # Backtracking: Try the current coin option as deep as possible
+                # Exclusion: When current option is explored fully, the stack pops and resets the parameter to previous states
                 dfs(remaining - coin, coin_count + 1)
         
         # Start DFS
