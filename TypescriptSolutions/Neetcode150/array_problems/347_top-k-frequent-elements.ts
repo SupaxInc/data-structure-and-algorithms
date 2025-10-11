1bfa423c047fd0663fcd408d511006dc6e0d1b48
SupaxInc <paulj_gonzales@hotmail.com>
2025-10-11 15:35:39 -0400

    small synax fixes

--- TypescriptSolutions/Neetcode150/array_problems/347_top-k-frequent-elements.ts
+++ TypescriptSolutions/Neetcode150/array_problems/347_top-k-frequent-elements.ts
@@ -1,13 +1,13 @@
 const topKFrequent = (nums: number[], k: number): number[] => {
     // + 1 since each index in array is a freq, so we need max number of freqs in an array
-    const freqs = new Array(nums.length+1).fill(null).map(() => []);
+    const freqs: number[][] = new Array(nums.length+1).fill(null).map(() => []);
 
     const count: Record<number, number> = {}; // (number, freq)
     for (let num of nums) count[num] = (count[num] || 0) + 1;
 
     for (let [key, val] of Object.entries(count)) freqs[val].push(Number(key));
 
-    const result = [];
+    const result: number[] = [];
     for (let i = freqs.length-1; i >= 0; i--) {
         const freqsVal = freqs[i];
 
