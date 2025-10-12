7df5893036ed59c76a31060288927ecc863c9e11
SupaxInc <paulj_gonzales@hotmail.com>
2025-10-12 15:41:07 -0400

    finished optimized 424

--- TypescriptSolutions/Neetcode150/sliding_window_problems/424_longest-repeating-character-replacement.ts
+++ TypescriptSolutions/Neetcode150/sliding_window_problems/424_longest-repeating-character-replacement.ts
@@ -18,3 +18,26 @@ const notOptimizedCharacterReplacement = (s: string, k: number): number => {
 
     return longestString;
 }
+
+const mostOptimizedCharacterReplacement = (s: string, k: number): number => {
+    let start = 0;
+    let longestString = 0;
+    let maxFreq = 0;
+    const n = s.length;
+    
+    const count: Record<string, number> = {};
+    for (let end = 0; end < n; end++) {
+        count[s[end]] = (count[s[end]] || 0) + 1;
+        // We actually just care about the max frequency
+        maxFreq = Math.max(count[s[end]], maxFreq);
+
+        while (start < end && ((end - start) + 1 - maxFreq) > k) {
+            count[s[start]]--;
+            start++;
+        }
+
+        longestString = Math.max(longestString, (end - start) + 1);
+    }
+
+    return longestString;
+}
