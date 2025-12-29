4d875d35f0d0fee8435a63c6870c9d820a863e29
SupaxInc <paulj_gonzales@hotmail.com>
2025-12-29 08:56:14 -0500

    more readable 121 ts

--- TypescriptSolutions/Neetcode150/sliding_window_problems/121_best-time-to-buy-and-sell-stock.ts
+++ TypescriptSolutions/Neetcode150/sliding_window_problems/121_best-time-to-buy-and-sell-stock.ts
@@ -21,3 +21,19 @@ const maxProfit = (prices: number[]): number => {
 
     return maxProfit;
 };
+
+const readableDynamicWindowMaxProfitSolution = (prices: number[]): number => {
+    let start = 0;
+    let maxProfit = 0;
+
+    for (let end = 1; end < prices.length; end++) {
+        if (prices[start] < prices[end]) {
+            maxProfit = Math.max(prices[end] - prices[start], maxProfit);
+        } else {
+            // Found a smaller stock price in the future so close dynamic window
+            start = end;
+        }
+    }
+
+    return maxProfit;
+};
