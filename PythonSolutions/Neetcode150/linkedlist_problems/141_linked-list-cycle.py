44b70a724e87c8401e370a44d081e0a5f8ee1ea4
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-01 22:01:12 -0500

    refining 141

--- PythonSolutions/Neetcode150/linkedlist_problems/141_linked-list-cycle.py
+++ PythonSolutions/Neetcode150/linkedlist_problems/141_linked-list-cycle.py
@@ -1,42 +1,19 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class MySolution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head:
-            return False
-
-        # Goes through the first iteration
-        slow, fast = head, head.next
-
-        while fast and fast.next:
-            # Check if there's a cycle for the beginning of each iteration
-            if slow == fast:
-                return True
-
-            slow = slow.next
-            # Fast node goes twice as fast as the slow node
-            fast = fast.next.next
-        
-        return False
+from typing import Optional
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head:
             return False
         
+        # Beginning, point slow and fast pointers to head
         slow = fast = head
 
-        # Checks for the next node as well to prevent hitting a null pointer in the fast variable
+        # Checks null for fast.next node as well to prevent hitting a null pointer when going double speed
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
