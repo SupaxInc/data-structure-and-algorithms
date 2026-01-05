7304df2cc6eec6a4452fdc04b6b15cacf06ac88e
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-05 09:06:23 -0500

    refined 36 py

--- PythonSolutions/Neetcode150/array_problems/36_valid-sudoku.py
+++ PythonSolutions/Neetcode150/array_problems/36_valid-sudoku.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = defaultdict(set) # Rule 1: Each row must contain the digits 1-9 without repetition.
@@ -9,6 +10,8 @@ class Solution:
             # iterate through the columns in each row [[., ., ., etc..]]
             for c in range(0, len(board[r])):
                 value = board[r][c]
+                if value == ".":
+                    continue
                 
                 # Checks if the value is anywhere within a certain 3x3 square out of the 9 squares
                 squareKey = (r//3, c//3) 
@@ -16,13 +19,13 @@ class Solution:
                 # Check if any of the rules are broken where 1-9 is found as duplicate
                 if value in rows[r] or value in cols[c] or value in squares[squareKey]:
                     return False
-                if value != ".":
-                    # This will add all nums that belongs one of the rows
-                    rows[r].add(value)
-                    # This will add all nums that belongs to one of the columns
-                    cols[c].add(value)
-                    # This will add all nums that belongs to one of the 3x3 squares
-                        # E.g. (2//3, 5//3) = (0, 1) = coords belongs to square 1, so we add all nums that are in square 1
-                    squares[squareKey].add(value)
+
+                 # This will add all nums that belongs one of the rows
+                rows[r].add(value)
+                # This will add all nums that belongs to one of the columns
+                cols[c].add(value)
+                # This will add all nums that belongs to one of the 3x3 squares
+                    # E.g. (2//3, 5//3) = (0, 1) = coords belongs to square 1, so we add all nums that are in square 1
+                squares[squareKey].add(value)
         
         return True
