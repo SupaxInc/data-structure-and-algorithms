2f6479902c98289d4c189e41a567cb141e548553
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-05 13:30:40 -0500

    refined 38 ts

--- TypescriptSolutions/Neetcode150/array_problems/36_valid-sudoku.ts
+++ TypescriptSolutions/Neetcode150/array_problems/36_valid-sudoku.ts
@@ -1,28 +1,30 @@
-function isValidSudoku(board: string[][]): boolean {
-    const rows: Record<number, Set<string>> = {};
-    const cols: Record<number, Set<string>> = {};
+const isValidSudoku = (board: string[][]): boolean =>  {
+    const rows: Record<string, Set<string>> = {};
+    const cols: Record<string, Set<string>> = {};
     const squares: Record<string, Set<string>> = {};
 
+    const ROWS = board.length;
+    const COLS = board[0].length;
 
-    for (let r = 0; r < board.length; r++){
-        for (let c = 0; c< board[r].length; c++) {
-            const value = board[r][c];
-            if (value == ".") continue;
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            const val = board[r][c];
+            if (val === ".") continue;
 
-            const squareKey: string = [Math.floor(r / 3), Math.floor(c/3)].toString();
+            const squareKey = [Math.floor(r / 3), Math.floor(c / 3)].toString();
             if (!rows[r]) rows[r] = new Set<string>();
             if (!cols[c]) cols[c] = new Set<string>();
             if (!squares[squareKey]) squares[squareKey] = new Set<string>();
 
-            if (rows[r].has(value) || cols[c].has(value) || squares[squareKey].has(value)) {
+            if (rows[r].has(val) || cols[c].has(val) || squares[squareKey].has(val)) {
                 return false;
             }
 
-            rows[r].add(value);
-            cols[c].add(value);
-            squares[squareKey].add(value);
+            rows[r].add(val);
+            cols[c].add(val);
+            squares[squareKey].add(val);
         }
     }
 
     return true;
-};
+}
