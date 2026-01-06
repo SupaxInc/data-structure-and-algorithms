6d3f36a66a9f949c5391af56920bdc6ab4c095d8
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-05 21:59:42 -0500

    finished 167 ts

---
+++ TypescriptSolutions/Neetcode150/pointer_problems/167_two-sum-2-input-sorted.ts
@@ -0,0 +1,20 @@
+const twoSum = (numbers: number[], target: number): number[] => {
+    let l = 0;
+    let r = numbers.length - 1;
+
+    while (l < r) {
+        const total = numbers[l] + numbers[r];
+
+        if (total === target) {
+            return [l + 1, r + 1];
+        }
+
+        if (total < target) {
+            l += 1;
+        } else {
+            r -= 1;
+        }
+    }
+
+    return [];
+};
