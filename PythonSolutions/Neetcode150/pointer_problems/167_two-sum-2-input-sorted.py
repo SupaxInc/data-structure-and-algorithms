e9a8a63f1257f96cb49210c5a5e8cabccd2e8447
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-05 21:48:56 -0500

    refined 167 py

--- PythonSolutions/Neetcode150/pointer_problems/167_two-sum-2-input-sorted.py
+++ PythonSolutions/Neetcode150/pointer_problems/167_two-sum-2-input-sorted.py
@@ -1,16 +1,19 @@
+from typing import List
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         l, r = 0, len(numbers) - 1
 
-        # Move right pointer to left if total is greater
-        # Move left pointer to right if total is smaller
         while l < r:
             total = numbers[l] + numbers[r]
+            
+            if total == target:
+                return [l + 1, r + 1]
+
+            # Move right pointer to left if total is greater
+            # Move left pointer to right if total is smaller
             if total < target:
                 l += 1
-            elif total > target:
-                r -= 1
             else:
-                return [l+1, r+1]
-        
+                r -= 1
+            
         return []
