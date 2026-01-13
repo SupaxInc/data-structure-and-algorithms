14fc887ee9431b0bd182025b5cb732f5605b5802
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-13 11:12:37 -0500

    refined 875 py

--- PythonSolutions/Neetcode150/binarysearch_problems/875_koko-eating-bananas.py
+++ PythonSolutions/Neetcode150/binarysearch_problems/875_koko-eating-bananas.py
@@ -1,33 +1,30 @@
 from typing import List
+from math import ceil
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # Low and high pointer will be the range of 1 to max amount of bananas in a pile
-        # This creates a K array to iterate through to help find the most min amount of bananas to eat per hour
-            # If max = 8 -> [1, 2, 3, 4, 5, 6, 7, 8]
+        # Create a sorted search space of (1... max of piles)
         lo, hi = 1, max(piles)
-
-        # Current min would be max value inside piles
-        # E.g. Max is 8 bananas Koko can eat per hour, so highest starts at 8 then we can go lower
+        # The most CURRENT min eating speed would be the highest max of the pile
         minK = hi
 
         while lo <= hi:
-            # The K value is how much Koko can eat in an hour, uses the new 1 to K array
-            midK = lo + ((hi - lo) // 2)
-            hours = 0
+            # Binary search the middle eating speed to divide and conquer
+            midK = lo + (hi - lo) // 2
 
-            for pile in piles:
-                # Calculate how long it takes Koko to eat the pile based on the banana (K) per hour
-                # Ceiling the value is needed to round up to the hour
-                # E.g. 4/5 = 0.85 = 1, pile of 4 bananas and Koko can eat 5 in an hour
-                hours += math.ceil(pile/midK)
+            # The hour amount we have used to eat all the piles based on midK
+            currH = 0
+            # piles / eating speed = piles can eat / hour
+            for p in piles:
+                # Need to ceil in the event that we eat a portion of the pile in an hour or eat all of it
+                    # e.g. 5/9 = 0.55 -> round to 1 since we are counting by hour
+                currH += ceil(p / midK)
             
-            # If Koko can eat the piles within the hour limit before the guards come, it will be new min
-                # Lower the higher boundary to try and find a smaller min to find a smaller amount of bananas Koko can eat in an hour
-            if hours <= h:
+            # If we were able to eat all piles in the hour limit then create new min
+            if currH <= h:
                 minK = min(minK, midK)
                 hi = midK - 1
-            # If Koko can't eat the piles within the hour limit, then go higher in bananas (K) eaten per hour
             else:
+                # If we couldn't eat the piles in the certain K, then check for a higher K limit
                 lo = midK + 1
-    
+        
         return minK
