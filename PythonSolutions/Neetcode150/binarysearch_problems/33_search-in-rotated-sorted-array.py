9cc71f45701a412d4eb03e0306f47c554c22dbe5
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-16 12:48:19 -0500

    refined 33 py

--- PythonSolutions/Neetcode150/binarysearch_problems/33_search-in-rotated-sorted-array.py
+++ PythonSolutions/Neetcode150/binarysearch_problems/33_search-in-rotated-sorted-array.py
@@ -1,31 +1,30 @@
+from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         lo, hi = 0, len(nums) - 1
 
         while lo <= hi:
-            mid = lo + ((hi - lo) // 2)
+            mid = lo + (hi - lo) // 2
 
             if nums[mid] == target:
-                # Return index
                 return mid
-            
-            # In left sorted portion of array
-            if nums[mid] > nums[hi]:
-                # Search right if array is rotated where the target is on the right portion of array
-                # Or if its still in left sorted portion but greater than our current mid number
+
+            # Using mid as pivot, we can find out if we are in left or right sorted portion
+            if nums[mid] > nums[hi]: # In left sorted portion
+                # Search in right of array if:
+                    # Target < Lo (target ends up in right sorted portion)
+                    # Target > mid (target is still in left sorted portion)
                 if target < nums[lo] or target > nums[mid]:
                     lo = mid + 1
-                # Search left if target is within the left sorted portion
                 else:
                     hi = mid - 1
-            # In right sorted portion of array
-            else:
-                # Search left if array is rotated where the target is on left portion of array
-                # Or if its still in right sorted portion but less than our current mid number
+            else: # In right sorted portion
+                # Search in left of array if:
+                    # Target > Hi (target ends up in left sorted portion)
+                    # Target < Mid (target is still in right sorted portion)
                 if target > nums[hi] or target < nums[mid]:
                     hi = mid - 1
-                # Search right if target is in right sorted portion
-                else:
+                else: 
                     lo = mid + 1
-
-        return -1 
+            
+        return -1
