c5893a08a4f6ef60299a4f9ff224e8323cb6ba8d
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-19 22:28:31 -0500

    added 235 ts

---
+++ TypescriptSolutions/Neetcode150/binary-tree_problems/235_lowest-common-ancestor-of-a-tree.ts
@@ -0,0 +1,25 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+
+const lowestCommonAncestor = (root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode => {
+    let curr = root;
+
+    while (curr) {
+        if (p.val < curr.val && q.val < curr.val) {
+            curr = curr.left;
+        } else if (p.val > curr.val && q.val > curr.val) {
+            curr = curr.right;
+        } else {
+            return curr;
+        }
+    }
+};
