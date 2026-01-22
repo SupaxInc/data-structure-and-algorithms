96ef891c991a2c1b5586d4bb829a694792087f0f
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-21 20:54:14 -0500

    refined 199 py

--- PythonSolutions/Neetcode150/binarytree_problems/199_binary-tree-right-side-view.py
+++ PythonSolutions/Neetcode150/binarytree_problems/199_binary-tree-right-side-view.py
@@ -1,34 +1,32 @@
+from typing import List
+from collections import deque
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
+    def rightSideView(self, root: TreeNode) -> List[int]:
         if not root:
-            return res
-
+            return []
+        
         queue = deque([root])
+        res = []
 
         while queue:
-            # Queue length should be outside of the for loop so we don't recalculate the size of the level per iteration in the loop
-            queueLength = len(queue)
+            n = len(queue)
 
-            # The for loop will only check the the nodes for the current level
-                # This is because queue length stays the same and does not change within the for loop
-                # It gets recalculate when for loop is done
-            for i in range(queueLength):
+            # Level order traversal
+            for i in range(n):
                 curr = queue.popleft()
-
-                # Only add last node of the level so its only grabbing the far right node from view
-                if i == queueLength - 1:
+                # In the current level, if we are at the last node add to result (gets right most value)
+                if i == n - 1:
                     res.append(curr.val)
-    
+                
                 if curr.left:
                     queue.append(curr.left)
                 if curr.right:
                     queue.append(curr.right)
-        
+    
         return res
