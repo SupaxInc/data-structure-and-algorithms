df6330bce673eb31fa7731c0a475af4e8a791cdf
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-21 20:08:24 -0500

    refining 102 py

--- PythonSolutions/Neetcode150/binarytree_problems/102_binary-tree-level-order-traversal.py
+++ PythonSolutions/Neetcode150/binarytree_problems/102_binary-tree-level-order-traversal.py
@@ -1,29 +1,34 @@
+from typing import List
+from collections import deque
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        result = []
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
-            return result
+            return []
 
-        queue = deque()
-        queue.append(root)
+        res = []
+        queue = deque([root])
 
         while queue:
             level = []
-            # Traverse through the entire level before moving to next
-            # Without a for loop, we would end up looking through next level
-            for _ in range(len(queue)):
+            n = len(queue)
+
+            # Length of queue wont be re-calculated inside the for loop, allows to process each level
+            for _ in range(n):
                 curr = queue.popleft() # FIFO
                 level.append(curr.val)
+
                 if curr.left:
                     queue.append(curr.left)
                 if curr.right:
                     queue.append(curr.right)
-            result.append(level)
-        
-        return result
+            
+            # Get the result of the specific level above
+            res.append(level)
+
+        return res
