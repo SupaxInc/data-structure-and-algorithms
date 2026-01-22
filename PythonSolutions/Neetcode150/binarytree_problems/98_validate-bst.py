5f6d628658dc6ace5fb665e20c47e3d3f355e2dd
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-22 09:01:02 -0500

    refined 98

--- PythonSolutions/Neetcode150/binarytree_problems/98_validate-bst.py
+++ PythonSolutions/Neetcode150/binarytree_problems/98_validate-bst.py
@@ -1,24 +1,27 @@
+from typing import Optional
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def validate(node, low=float("-Inf"), high=float("Inf")):
-            # Base case 1: No more nodes to traverse to, therefore, no issues finding ranges
+        if not root:
+            return True
+        
+        # Preorder traversal, uses params to know lowest and highest in current path
+        def validate(node, low, high):
             if not node:
                 return True
             
-            # Base case 2: Check if current node value is in the correct range
+            # Fail if current node is not in the range of lowest and highest in path
             if not (low < node.val < high):
                 return False
             
             # Going left -> Pass current node value as new high
-                # Since all values to the left of the node should be lower than the ancestor (current node)
             # Going right -> Pass current node value as new low
-                # Since all values to the right of the node should be higher than the ancestor (current node)
             return validate(node.left, low, node.val) and validate(node.right, node.val, high)
         
-        return validate(root)
+        # Lowest and highest in the currernt path at the beginning are infinites
+        return validate(root, float("-inf"), float("inf"))
