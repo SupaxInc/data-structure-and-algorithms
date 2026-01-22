e4f3a3c5ad2aedf2bc98c76743aed0d64919e119
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-21 21:26:54 -0500

    refined 1448 py

--- PythonSolutions/Neetcode150/binarytree_problems/1448_count-good-nodes-in-binary-tree.py
+++ PythonSolutions/Neetcode150/binarytree_problems/1448_count-good-nodes-in-binary-tree.py
@@ -1,3 +1,4 @@
+from typing import Optional
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -5,7 +6,7 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
+    def goodNodes(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
         
