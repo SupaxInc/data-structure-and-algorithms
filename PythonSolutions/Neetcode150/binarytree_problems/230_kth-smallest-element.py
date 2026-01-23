7ffc74e0eb908e9017170dcf5623c206c80bdd06
SupaxInc <paulj_gonzales@hotmail.com>
2026-01-22 22:31:35 -0500

    refined 230

--- PythonSolutions/Neetcode150/binarytree_problems/230_kth-smallest-element.py
+++ PythonSolutions/Neetcode150/binarytree_problems/230_kth-smallest-element.py
@@ -1,68 +1,37 @@
+from typing import Optional
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class RecursiveSolution:
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # Keep track of count
-        # We can't pass it in the parameters since the count will be recursively called during in-order
-        self.count = 0 
+        self.count = 0
 
-        def inorder(node):
-            # Base case 1: Hit the end leaf node
+        # Inorder traversal gives us numbers in order in a BST
+        def inorderDFS(node):
             if not node:
-                return None
-            
-            # Receives either None or the value of kth smallest element
-            leftVal = inorder(node.left)
-            # Propagate value up if value was found
-            if leftVal is not None:
-                return leftVal
-            
+                return -1
+
+            leftVal = inorderDFS(node.left)
+            # At some point when we travel to left subtree, it'll return the actual value
+            # during the inorder traversal
+            if leftVal != -1:
+                return leftVal 
+
             self.count += 1
+            # Hitting Kth, just return current node value and propagate it up the tree
             if self.count == k:
                 return node.val
             
-            rightVal = inorder(node.right)
-            if rightVal is not None:
+            # During in order traversal of right subtree, the Kth may be hit
+            rightVal = inorderDFS(node.right)
+            if rightVal != -1:
                 return rightVal
-            
-            # Return a None if kth smallest element was found
-            return None
+
+            # Return -1 if we don't find anything after traversing through all subtrees
+            return -1
         
-        return inorder(root)
+        return inorderDFS(root)
             
-
-            
-
-    
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        if not root:
-            return None
-        count = 0
-        stack = []
-        curr = root
-        while stack or curr:
-            # Go as left as possible
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-            # Current should be none at this point
-                # So we pop the stack, in which is no longer None 
-                # Since we now have a new node from the stack
-            curr = stack.pop()
-            # Inorder case
-            count += 1
-            if count == k:
-                return curr.val
-            # Visited left sub tree now go right
-            curr = curr.right
