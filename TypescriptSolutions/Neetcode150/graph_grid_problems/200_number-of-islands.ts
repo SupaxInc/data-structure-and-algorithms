c2a6d45785d7b9fea54a91c1de7f5f5c285dc427
SupaxInc <paulj_gonzales@hotmail.com>
2026-02-01 00:33:58 -0500

    modified 200 ts"

--- TypescriptSolutions/Neetcode150/graph_grid_problems/200_number-of-islands.ts
+++ TypescriptSolutions/Neetcode150/graph_grid_problems/200_number-of-islands.ts
@@ -1,4 +1,7 @@
 export {};
+
+import { Queue } from "@datastructures-js/queue";
+
 const numIslandsDFS = (grid: string[][]): number => {
     const ROWS = grid.length;
     const COLS = grid[0].length;
@@ -28,6 +31,44 @@ const numIslandsDFS = (grid: string[][]): number => {
             if (grid[r][c] === "1") {
                 dfs(r, c);
 
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+};
+
+const numIslandsBFS = (grid: string[][]): number => { 
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+    const DIRS = [[0, 1], [1, 0], [-1, 0], [0, -1]];
+    
+    const bfs = (row, col): void => {
+        const queue = new Queue<[number, number]>([[row, col]]);
+
+        while (!queue.isEmpty()) {
+            const [r, c] = queue.dequeue();
+
+            if (r < 0 || c < 0 || r > ROWS - 1 || c > COLS - 1) {
+                continue;
+            }
+
+            if (grid[r][c] === "0") continue;
+
+            grid[r][c] = "0";
+
+            for (const [dx, dy] of DIRS) {
+                queue.enqueue([r + dx, c + dy]);
+            }
+        }
+    };
+
+    let count = 0;
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            if (grid[r][c] === "1") {
+                bfs(r, c);
                 count += 1;
             }
         }
