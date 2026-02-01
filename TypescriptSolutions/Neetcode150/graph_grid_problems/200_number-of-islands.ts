86a0d7972650162bc68c58efa1f6cf0a1c141688
SupaxInc <paulj_gonzales@hotmail.com>
2026-02-01 00:19:04 -0500

    added 200 ts

---
+++ TypescriptSolutions/Neetcode150/graph_grid_problems/200_number-of-islands.ts
@@ -0,0 +1,37 @@
+export {};
+const numIslandsDFS = (grid: string[][]): number => {
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+    const DIRS: number[][] = [[0, 1], [1, 0], [-1, 0], [0, -1]];
+
+    const dfs = (row, col) => {
+        if (row < 0 || col < 0 || row > ROWS - 1 || col > COLS - 1) {
+            return;
+        }
+
+        if (grid[row][col] === "0") {
+            return;
+        }
+
+        grid[row][col] = "0";
+
+        for (const [dx, dy] of DIRS) {
+            dfs(row + dx, col + dy);
+        }
+
+        return;
+    };
+
+    let count = 0;
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            if (grid[r][c] === "1") {
+                dfs(r, c);
+
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+};
