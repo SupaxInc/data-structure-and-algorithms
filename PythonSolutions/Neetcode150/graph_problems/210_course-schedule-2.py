ed04a49ceefa1d363ed2c7d08018617d7fba6ee3
SupaxInc <paulj_gonzales@hotmail.com>
2026-02-04 23:03:53 -0500

    210 refined py

--- PythonSolutions/Neetcode150/graph_problems/210_course-schedule-2.py
+++ PythonSolutions/Neetcode150/graph_problems/210_course-schedule-2.py
@@ -2,49 +2,38 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         preMap = defaultdict(list)
 
-        # Create an adjacency list where courses -> prerequisites
-            # crs is pointing to pre because it answers "what do I need to complete before taking the course"
-                # Allows us to check what we need to complete first before taking a course
-                # *Think about how it "backtracks", we complete the course when we backtrack so it goes backwards*
-            # If we flip it around, it answers instead "what courses does this unlock"
+        # Map course -> pre-req as the adjacency list
+            # Think about how it backtracks when call stack pops
+            # We visit courses as deep as possible after visiting it pops, and we add that deepest pre-req node first
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
-
-        visited, completed = set(), set()
-        order = []
         
-        def dfs(crs):
-            # Base case 1: Found a visited node in current path, cycle detected
-            if crs in visited:
-                return False
+        visit = set()
+        done = set()
+        #! Sets aren't ordered in Python so we use a list
+        order = []
 
-            # Base case 2: Found an already completed course
-            if crs in completed:
+        def dfs(crs):
+            if crs in visit:
+                return False
+            if crs in done:
                 return True
             
-            # Visit the course node
-            visited.add(crs)
+            visit.add(crs)
 
-            # Explore all neighbours (prereqs) for current course node
+            # visit the pre-requisites as deep as possible
             for pre in preMap[crs]:
-                # Found an invalid path, pop the stack and return False
                 if not dfs(pre):
                     return False
             
-            # Unvisit the course node
-                # Essentially "completing" the course, so we go backwards and explore other courses
-            visited.remove(crs)
-            completed.add(crs)
+            # We backtrack here. Pre-requisites will be added first in the order.
+            visit.remove(crs)
+            done.add(crs)
+            order.append(crs)
 
-            # Add the ordering during unvisit, remember when we backtrack, we "complete" that course
-            order.append(crs) 
-
-            # All current paths valid return a True
             return True
-            
         
-        # *Traverse all courses because there may be disconnected components*
-            # 1 DFS is not enough, this is why when we backtrack we unvisit the node so we can test the other courses in the case that its part of another graph
+        # Visit all courses in course schedule
         for crs in range(numCourses):
             if not dfs(crs):
                 return []
