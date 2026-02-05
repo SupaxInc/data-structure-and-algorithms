9c6d8587b9eabdd809ec54e928975b822acbfd56
SupaxInc <paulj_gonzales@hotmail.com>
2026-02-04 23:04:17 -0500

    210 refined py

--- PythonSolutions/Neetcode150/graph_problems/210_course-schedule-2.py
+++ PythonSolutions/Neetcode150/graph_problems/210_course-schedule-2.py
@@ -1,3 +1,5 @@
+from typing import List
+from collections import defaultdict
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         preMap = defaultdict(list)
