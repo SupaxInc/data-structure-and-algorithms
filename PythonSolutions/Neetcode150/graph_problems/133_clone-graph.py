cf04e3b85037fdba1458121a7fe8e9c3bcaa5f70
SupaxInc <paulj_gonzales@hotmail.com>
2026-06-12 18:30:52 -0400

    linting

--- PythonSolutions/Neetcode150/graph_problems/133_clone-graph.py
+++ PythonSolutions/Neetcode150/graph_problems/133_clone-graph.py
@@ -1,33 +1,35 @@
+from typing import Optional
+
 
 # Definition for a Node.
 class Node:
-    def __init__(self, val = 0, neighbors = None):
+    def __init__(self, val=0, neighbors=None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
 
-from typing import Optional
 class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
         if not node:
             return None
-        
+
         oldToNew = {}
 
         def dfs(curr):
             # Base case: If node is already mapped, return the copied node
             if curr in oldToNew:
                 return oldToNew[curr]
-            
-            copyNode = Node(curr.val) # Copy current node
-            oldToNew[curr] = copyNode # Map current node to new copy
+
+            copyNode = Node(curr.val)  # Copy current node
+            oldToNew[curr] = copyNode  # Map current node to new copy
 
             # Go through all of neighbours for current node
             for nei in curr.neighbors:
                 # Add the current nodes neighbors to the copied node
                 copyNode.neighbors.append(dfs(nei))
-            
+
             # Need to return copy node in the event base case is not hit for first time visits
             return copyNode
-            
+
         return dfs(node)
+
