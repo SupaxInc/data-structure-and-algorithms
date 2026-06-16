84ac7b8157780d55f9838fba6ccbed1ff220bd20
SupaxInc <paulj_gonzales@hotmail.com>
2026-06-15 23:05:58 -0400

    lint 242

--- PythonSolutions/Neetcode150/array_problems/242_valid-anagram.py
+++ PythonSolutions/Neetcode150/array_problems/242_valid-anagram.py
@@ -7,7 +7,7 @@ class Solution1:
                 letterMap[char] = 1
             else:
                 letterMap[char] += 1
-        
+
         return letterMap
 
     def isAnagram(self, s: str, t: str) -> bool:
@@ -17,40 +17,45 @@ class Solution1:
         # In python you can compare sets with another set easily due to built in operators
         if set(sMap.keys()) != set(tMap.keys()):
             return False
-        
+
         for key in sMap:
             if sMap[key] != tMap[key]:
                 return False
-        
+
         return True
-    
+
+
 class Solution2:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
+
         countS, countT = {}, {}
 
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
-        
+
         # You can also compare hashmaps with another hashmap, it compares both keys and values
         return countS == countT
 
+
 from collections import defaultdict
+
+
 class Solution3:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-            
+
         countS = defaultdict(int)
         countT = defaultdict(int)
 
         for char in s:
             countS[char] += 1
-        
+
         for char in t:
             countT[char] += 1
-        
+
         return countS == countT
+
