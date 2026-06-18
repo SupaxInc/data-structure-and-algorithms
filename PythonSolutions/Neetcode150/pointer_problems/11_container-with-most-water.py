5876dd6be378113488cb797105f08c5f3e5abb86
SupaxInc <paulj_gonzales@hotmail.com>
2026-06-17 22:31:55 -0400

    lint 11

--- PythonSolutions/Neetcode150/pointer_problems/11_container-with-most-water.py
+++ PythonSolutions/Neetcode150/pointer_problems/11_container-with-most-water.py
@@ -1,4 +1,6 @@
 from typing import List
+
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         l, r = 0, len(height) - 1
@@ -6,14 +8,15 @@ class Solution:
 
         while l < r:
             # Get min height so water doesn't overflow
-            area = min(height[l], height[r]) * (r-l)
+            area = min(height[l], height[r]) * (r - l)
             maxArea = max(maxArea, area)
 
             # If the height on the left is smaller then find a new height for left
-                # We want to maximize the highest chance of finding the highest combinations of height
+            # We want to maximize the highest chance of finding the highest combinations of height
             if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
-        
+
         return maxArea
+
