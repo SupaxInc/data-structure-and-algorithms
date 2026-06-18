ddd6a38067984ea325f871384371fcf57835586f
SupaxInc <paulj_gonzales@hotmail.com>
2026-06-17 22:24:31 -0400

    lint 567 and comment updates

--- PythonSolutions/Neetcode150/slidingwindow_problems/567_permutation-in-string.py
+++ PythonSolutions/Neetcode150/slidingwindow_problems/567_permutation-in-string.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+
+
 class MySolution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         windowLength = len(s1)
@@ -8,17 +10,17 @@ class MySolution:
         s1Count = defaultdict(int)
         s2Count = defaultdict(int)
         start = 0
-    
-        for char in s1: # O(n)
-            s1Count [char] += 1
 
-        for end in range(0, len(s2)): # O(m)
+        for char in s1:  # O(n)
+            s1Count[char] += 1
+
+        for end in range(0, len(s2)):  # O(m)
             s2Count[s2[end]] += 1
 
             # Check for criterias when its finally at its fixed window length
             if end >= windowLength - 1:
                 # If the char occurences are the same for both count maps then there are permutations
-                if s1Count == s2Count: # O(26)
+                if s1Count == s2Count:  # O(26)
                     return True
 
                 # Move the fixed size window by decrementing the count
@@ -28,12 +30,13 @@ class MySolution:
                 if s2Count[s2[start]] == 0:
                     # Can use delete keyword here instead of pop
                     s2Count.pop(s2[start])
-            
+
                 # Increment the start pointer so it stays within fixed window length
                 start += 1
 
         return False
-    
+
+
 class MostOptimalSolution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
@@ -46,26 +49,28 @@ class MostOptimalSolution:
         matches = 0
         start = 0
         for end in range(len(s2)):
-            # Add the new character to the window
+            # Add the new character to the window by removing it from the map
             if s2[end] in s1Count:
                 s1Count[s2[end]] -= 1
                 # If the frequency of the character is 0, then we increment the matches as its a match for the permutation
+                # as we were able to get all the characters from first string
                 if s1Count[s2[end]] == 0:
                     matches += 1
-            
+
             # Remove the character leaving the window if we are at fixed window size
-            if (end-start) + 1 > len(s1):
+            if (end - start) + 1 > len(s1):
                 if s2[start] in s1Count:
                     # If the frequency of the start character was 0, then we decrement the matches as its no longer a match
                     if s1Count[s2[start]] == 0:
                         matches -= 1
                     s1Count[s2[start]] += 1
-                
+
                 # Increment the start pointer so it stays within fixed window length
                 start += 1
-                
+
             # If all characters match, return True
             if matches == len(s1Count):
                 return True
-        
+
         return False
+
