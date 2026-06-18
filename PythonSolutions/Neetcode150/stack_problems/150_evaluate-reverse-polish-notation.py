319ae35ce6173a44cc1f92b892ec257660337f6d
SupaxInc <paulj_gonzales@hotmail.com>
2026-06-18 12:29:15 -0400

    lint 150

--- PythonSolutions/Neetcode150/stack_problems/150_evaluate-reverse-polish-notation.py
+++ PythonSolutions/Neetcode150/stack_problems/150_evaluate-reverse-polish-notation.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
@@ -5,23 +8,24 @@ class Solution:
             # If you see a number, push it to the stack
             # Can't use isdigit() as it only checks from 0-9
             if token not in "+-*/":
-                stack.append(int(token)) # Convert token string to int
+                stack.append(int(token))  # Convert token string to int
             else:
                 # In postfix notation, the LAST number added to stack is the right operand
                 right = stack.pop()
                 # The second last number added to stack is the left operand
                 left = stack.pop()
-                if token == '+':
+                if token == "+":
                     stack.append(left + right)
-                elif token == '-':
+                elif token == "-":
                     stack.append(left - right)
-                elif token == '*':
+                elif token == "*":
                     stack.append(left * right)
-                elif token == '/':
+                elif token == "/":
                     # Dividing left and right would create a floating point number
                     # We convert it to an int so that it truncates to 0 (e.g -3.5 to -3)
                     # Using // would not work here as it truncates to -Infinity (e.g -3.5 to -4)
                     stack.append(int(left / right))
-                    
+
         # Final result is the last operation added to stack
         return stack[-1]
+
