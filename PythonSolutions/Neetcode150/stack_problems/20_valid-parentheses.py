8578a7ce1f6fda01f640b9491c7c0827173daeac
SupaxInc <paulj_gonzales@hotmail.com>
2026-06-18 11:41:26 -0400

    lint 20

--- PythonSolutions/Neetcode150/stack_problems/20_valid-parentheses.py
+++ PythonSolutions/Neetcode150/stack_problems/20_valid-parentheses.py
@@ -1,12 +1,8 @@
 class MoreReadableSolution:
     def isValid(self, s: str) -> bool:
-        complement = {
-            ']' :  '[',
-            ')' : '(',
-            '}' : '{'
-        }
+        complement = {"]": "[", ")": "(", "}": "{"}
         stack = []
-        
+
         for p in s:
             # If its a closing bracket
             if p in complement:
@@ -16,14 +12,15 @@ class MoreReadableSolution:
                     return False
             else:
                 stack.append(p)
-        
+
         # Ensure that the stack is exhausted or else we had a mismatched opening bracket
         return len(stack) == 0
+
 
 class Solution:
     def isValid(self, s: str) -> bool:
         # We need a way to figure out which brackets are closing brackets and what open brackets it matches to
-        mapDict = { '}' : '{', ']' : '[', ')' : '(' }
+        mapDict = {"}": "{", "]": "[", ")": "("}
         stack = []
 
         for char in s:
@@ -31,16 +28,17 @@ class Solution:
             if char not in mapDict:
                 # Stack is important here to know what the last opening bracket was to match with the first closing bracket
                 stack.append(char)
-                continue # Skip iteration so we don't pop the stack of closing brackets
-            
+                continue  # Skip iteration so we don't pop the stack of closing brackets
+
             # False if:
-                # Stack is empty, therefore, the closing bracket cannot be matched to an open bracket
-                # LAST opening bracket does not match the FIRST closing bracket
+            # Stack is empty, therefore, the closing bracket cannot be matched to an open bracket
+            # LAST opening bracket does not match the FIRST closing bracket
             elif not stack or stack[-1] != mapDict[char]:
                 return False
-            
+
             # Pop the stack when a closing bracket has been found (the elif was skipped)
             stack.pop()
-        
+
         # Handle edge case where all opening brackets were added or not all opening brackets were matched
         return not stack
+
