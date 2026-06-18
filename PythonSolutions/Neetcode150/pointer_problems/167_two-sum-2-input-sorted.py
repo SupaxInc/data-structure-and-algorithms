47c0647da86f7208890b7f814dd9fe5ffbfcca96
SupaxInc <paulj_gonzales@hotmail.com>
2026-06-17 22:51:39 -0400

    167 lint

--- PythonSolutions/Neetcode150/pointer_problems/167_two-sum-2-input-sorted.py
+++ PythonSolutions/Neetcode150/pointer_problems/167_two-sum-2-input-sorted.py
@@ -1,11 +1,13 @@
 from typing import List
+
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         l, r = 0, len(numbers) - 1
 
         while l < r:
             total = numbers[l] + numbers[r]
-            
+
             if total == target:
                 return [l + 1, r + 1]
 
@@ -15,5 +17,5 @@ class Solution:
                 l += 1
             else:
                 r -= 1
-            
+
         return []
