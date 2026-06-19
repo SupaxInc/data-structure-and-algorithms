af599908afd298eb24c9c359620a1f7bdf00328a
SupaxInc <paulj_gonzales@hotmail.com>
2026-06-18 22:29:12 -0400

    lint 127

--- PythonSolutions/Neetcode150/graph_problems/127_word-ladder.py
+++ PythonSolutions/Neetcode150/graph_problems/127_word-ladder.py
@@ -1,3 +1,7 @@
+from collections import defaultdict, deque
+from typing import List
+
+
 class OptimizedSolution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
@@ -14,15 +18,15 @@ class OptimizedSolution:
         for word in wordSet:
             for i in range(len(word)):
                 # Create the wildcard pattern
-                    # [:i]   -> grab letters in the word before the index
-                    # "*"    -> add a wild card for each index
-                    # [i+1:] -> grab letters in the word after the index
+                # [:i]   -> grab letters in the word before the index
+                # "*"    -> add a wild card for each index
+                # [i+1:] -> grab letters in the word after the index (since its replaced with a *)
                 # e.g. *ot, h*t, ho*
-                pattern = word[:i] + "*" + word[i+1:]
+                pattern = word[:i] + "*" + word[i + 1 :]
 
                 # Construct the graph
                 graph[pattern].append(word)
-        
+
         # Begin the search using the beginning word
         queue = deque([beginWord])
         visited = set()
@@ -39,19 +43,20 @@ class OptimizedSolution:
                 # Base case: We found the ending word, stop searching
                 if currWord == endWord:
                     return count
-                
+
                 for i in range(len(currWord)):
-                    pattern = currWord[:i] + "*" + currWord[i+1:]
+                    pattern = currWord[:i] + "*" + currWord[i + 1 :]
                     # Explore each adjacent neighbour for current pattern
                     for neiWord in graph[pattern]:
                         # Optimization: Check if neighbour word is not in visited before processing it, prevents dequeuing same words multiple times
                         if neiWord not in visited:
                             visited.add(neiWord)
                             queue.append(neiWord)
-            
+
             count += 1
 
         return 0
+
 
 class NonOptimizedSolution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
@@ -69,15 +74,15 @@ class NonOptimizedSolution:
         for word in wordSet:
             for i in range(len(word)):
                 # Create the wildcard pattern
-                    # [:i]   -> grab letters in the word before the index
-                    # "*"    -> add a wild card for each index
-                    # [i+1:] -> grab letters in the word after the index
+                # [:i]   -> grab letters in the word before the index
+                # "*"    -> add a wild card for each index
+                # [i+1:] -> grab letters in the word after the index
                 # e.g. *ot, h*t, ho*
-                pattern = word[:i] + "*" + word[i+1:]
+                pattern = word[:i] + "*" + word[i + 1 :]
 
                 # Construct the graph
                 graph[pattern].append(word)
-        
+
         # Begin the search using the beginning word
         queue = deque([beginWord])
         visited = set()
@@ -98,16 +103,17 @@ class NonOptimizedSolution:
                 # Base case 2: We found the ending word, stop searching
                 if currWord == endWord:
                     return count
-                
+
                 # Visit the current word node
                 visited.add(currWord)
-                
+
                 for i in range(len(currWord)):
-                    pattern = currWord[:i] + "*" + currWord[i+1:]
+                    pattern = currWord[:i] + "*" + currWord[i + 1 :]
                     # Explore each adjacent neighbour for current pattern
                     for neiWord in graph[pattern]:
                         queue.append(neiWord)
-            
+
             count += 1
 
         return 0
+
