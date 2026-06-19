758389636e9b78ade9d2c50a6f6c9d6b7c34912b
SupaxInc <paulj_gonzales@hotmail.com>
2026-06-18 23:20:17 -0400

    lint 417

--- PythonSolutions/Neetcode150/graph_problems/417_pacific-atlantic-water-flow.py
+++ PythonSolutions/Neetcode150/graph_problems/417_pacific-atlantic-water-flow.py
@@ -1,13 +1,15 @@
 from typing import List
+
+
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         self.ROWS, self.COLS = len(heights), len(heights[0])
         self.DIRS = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         self.heights = heights
-        
+
         pacific: set[tuple[int, int]] = set()
         atlantic: set[tuple[int, int]] = set()
-        
+
         # Multi-source DFS below: Edges are adjacent to ocean so these cells can already go to that specific ocean
 
         # DFS through upper pacific and lower atlantic oceans
@@ -26,26 +28,27 @@ class Solution:
             for col in range(self.COLS):
                 if (row, col) in pacific and (row, col) in atlantic:
                     res.append([row, col])
-        
+
         return res
-    
+
     def dfs(self, row, col, ocean):
         # Base case 1: We have already traversed here previously in another DFS so skip
-            # We do not need to do another DFS in a traversed cell since we already know at that point if water can flow
+        # We do not need to do another DFS in a traversed cell since we already know at that point if water can flow
         if (row, col) in ocean:
             return
-        
+
         # Mark that the water can flow to the ocean
         ocean.add((row, col))
-        
+
         for dx, dy in self.DIRS:
             nr, nc = row + dx, col + dy
 
             if nr < 0 or nc < 0 or nr > self.ROWS - 1 or nc > self.COLS - 1:
                 continue
-            
+
             # Only DFS the heights greater than current cell (we are following opposite of rain flow from cell)
             if self.heights[nr][nc] >= self.heights[row][col]:
                 self.dfs(nr, nc, ocean)
-        
+
         return
+
