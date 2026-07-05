4a52856b75bd8dfebd0bb15aaf8ce75f50c81dd5
SupaxInc <paulj_gonzales@hotmail.com>
2026-07-05 15:36:45 -0400

    lint 261

--- PythonSolutions/Neetcode150/graph_problems/261_valid-tree.py
+++ PythonSolutions/Neetcode150/graph_problems/261_valid-tree.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from typing import List
+
+
 class DFSSolution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         graph = defaultdict(list)
@@ -6,8 +10,9 @@ class DFSSolution:
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
-        
+
         visited = set()
+
         def dfs(node, prev):
             """
             Prev is used to keep track of what the previous node we was that we just traversed from.
@@ -16,7 +21,7 @@ class DFSSolution:
             # Base case 1: There is a cycle
             if node in visited:
                 return False
-            
+
             # Visit the vertex
             visited.add(node)
 
@@ -25,25 +30,26 @@ class DFSSolution:
                 # Base case 2: Skip an incorrect cycle detection from previous node
                 if nei == prev:
                     continue
-                
+
                 # Explore the current neighbor and make the current node the new previous node
                 if not dfs(nei, node):
                     return False
 
             # No cycle was found, return True
             return True
-        
+
         # Not valid tree check 1: There is a cycle in the graph
-            # - Begins search at node 0
-            # - Previous node is -1 which doesn't exist yet since there is no current prev node
-            # - Only 1 DFS call is needed since we don't care about disconnected components
+        # - Begins search at node 0
+        # - Previous node is -1 which doesn't exist yet since there is no current prev node
+        # - Only 1 DFS call is needed since we don't care about disconnected components
         if not dfs(0, -1):
             return False
-        
+
         # Not valid tree check 2: There are more than 1 component
-            # Checks if we've visited the correct amount of nodes compared to the size of nodes
-            # If we have not visited all nodes, then that means that are more than 1 components
+        # Checks if we've visited the correct amount of nodes compared to the size of nodes
+        # If we have not visited all nodes, then that means that are more than 1 components
         return len(visited) == n
+
 
 class UnionFind:
     def __init__(self, size):
@@ -57,9 +63,9 @@ class UnionFind:
         # Check if it is a root of itsself, if not flatten the tree so its easier to search
         if self.root[x] != x:
             self.root[x] = self.find(self.root[x])
-        
+
         return self.root[x]
-    
+
     def union(self, x, y):
         rootX = self.find(x)
         rootY = self.find(y)
@@ -67,7 +73,7 @@ class UnionFind:
         # Cycle detection: Check if the nodes we are connecting are in the same component
         if rootX == rootY:
             return False
-        
+
         if self.rank[rootX] > self.rank[rootY]:
             self.root[rootY] = rootX
         elif self.rank[rootY] > self.rank[rootX]:
@@ -75,9 +81,11 @@ class UnionFind:
         else:
             self.root[rootY] = rootX
             self.rank[rootX] += 1
-        
+
         # Return True since no cycle was detected
         return True
+
+
 class UnionFindSolution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         uf = UnionFind(n)
@@ -86,9 +94,9 @@ class UnionFindSolution:
             # Valid tree check 1: Check if theres any cycles between the union of two nodes
             if not uf.union(u, v):
                 return False
-        
+
         # Valid tree check 2: Ensure that there are only 1 root node (1 component)
-            # - Since the path has been compressed there should only be 1 root node for 1 component
-            # - If [1, 1, 1, 1] as an example, it is all the same root nodes set([1, 1, 1, 1]) == length 1, so only 1 connection
+        # - Since the path has been compressed there should only be 1 root node for 1 component
+        # - If [1, 1, 1, 1] as an example, it is all the same root nodes set([1, 1, 1, 1]) == length 1, so only 1 connection
         return len(set([uf.find(i) for i in range(n)])) == 1
-        
+
