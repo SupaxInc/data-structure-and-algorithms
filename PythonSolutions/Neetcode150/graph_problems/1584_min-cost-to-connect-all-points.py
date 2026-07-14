f3d2575f63cd9b4ec89b291a9f9d59bdc56784f9
SupaxInc <paulj_gonzales@hotmail.com>
2026-07-13 20:50:46 -0400

    1584 updated prim solution with better comments

--- PythonSolutions/Neetcode150/graph_problems/1584_min-cost-to-connect-all-points.py
+++ PythonSolutions/Neetcode150/graph_problems/1584_min-cost-to-connect-all-points.py
@@ -1,25 +1,29 @@
+import heapq
+from typing import List
+
+
 class PrimSolution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        # *This problem is a COMPLETE graph*
-            # Meaning that all points can connect with each other
-            # Therefore, we are not limited only to the vertices that we have visited
-                # Not like exploring with graph[neiPoint] instead visiting all vertices with range(totalVertices)
-            # We can actually visit any vertex that we want
+        # *This problem is a COMPLETE graph*, means that all points can connect with each other
+        # We are also given no edges in this problem.
+        # Therefore, we are not limited only to the vertices that we have visited
+        # Not like exploring with graph[neiPoint] instead we visit all vertices with range(totalVertices)
         totalVertices = len(points)
         visited = set()
         cost = 0
 
         # Typically in prim's algorithm, you want to start at point 0 but this can be a random point
-        minHeap = [(0, 0)] # (current cost, current point)
+        minHeap = [(0, 0)]  # (current cost, current point index)
 
         # Calculate the manhattan distance between two points (x1 - x2) + (y1 - y2)
         # This will be used as the cost of the points
+        # REMEMBER i, j is the index of the points
         def manhattan(i, j):
-            return abs(points[i][0] - points[j][0]) + abs(points[i][1]-points[j][1])
+            return abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
 
         # Stop the BFS when we hit E = V - 1
         while len(visited) < totalVertices:
-            # Get the lowest cost point in heap
+            # Get the lowest cost point in heap this includes ALL previous nodes calculation (all points are connected)
             currCost, currPoint = heapq.heappop(minHeap)
 
             # Even though we only explore vertices that have not been visited (see exploration below)
@@ -27,46 +31,21 @@ class PrimSolution:
             if currPoint in visited:
                 continue
 
-            # Visit the vertex
+            # Visit the vertex and connect it to our lowest cost graph
             cost += currCost
             visited.add(currPoint)
 
             # Explore ALL the vertices and calculate ALL of the cost for each vertices
-                # It is a COMPLETE graph we can visit any vertex
-                # neiPoint is an index in the points list which matches to a vertex
-                # e.g. points = [[0,0],[2,2]], neiPoint = 1 is vertex 1 which is point [2, 2]
+            # neiPoint is an index in the points list which matches to a vertex
+            # e.g. points = [[0,0],[2,2]], neiPoint = 1 is vertex 1 which is point [2, 2]
             for neiPoint in range(totalVertices):
                 # Only explore and calculate the cost of the points that have not been visited
                 if neiPoint not in visited:
+                    # Add all costs for each vertex that connects to current point we are on
                     heapq.heappush(minHeap, (manhattan(currPoint, neiPoint), neiPoint))
 
         return cost
-    
 
-class UnionFind:
-    def __init__(self, size):
-        self.parent = list(range(size))
-        self.rank = [1] * size
-    
-    def find(self, p):
-        if self.parent[p] != p:
-            self.parent[p] = self.find(self.parent[p])  # Path compression
-        return self.parent[p]
-    
-    def union(self, p, q):
-        rootP = self.find(p)
-        rootQ = self.find(q)
-        if rootP != rootQ:
-            # Union by rank
-            if self.rank[rootP] > self.rank[rootQ]:
-                self.parent[rootQ] = rootP
-            elif self.rank[rootP] < self.rank[rootQ]:
-                self.parent[rootP] = rootQ
-            else:
-                self.parent[rootQ] = rootP
-                self.rank[rootP] += 1
-            return True
-        return False
 
 class UnionFind:
     def __init__(self, size):
@@ -77,14 +56,14 @@ class UnionFind:
         if self.root[x] != x:
             self.root[x] = self.find(self.root[x])
         return self.root[x]
-    
+
     def union(self, x, y):
         rootX = self.find(x)
         rootY = self.find(y)
 
         if rootX == rootY:
             return False
-        
+
         if self.rank[rootX] > self.rank[rootY]:
             self.root[rootY] = rootX
         elif self.rank[rootY] > self.rank[rootX]:
@@ -92,10 +71,11 @@ class UnionFind:
         else:
             self.root[rootY] = rootX
             self.rank[rootX] += 1
-        
+
         return True
 
-class UnionFindSolution:
+
+class KruskalSolution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         # Each point is a vertex on the complete graph
         totalVertices = len(points)
@@ -105,9 +85,11 @@ class UnionFindSolution:
         for i in range(totalVertices):
             # Need to compare i (x[i], y[i]) with every j (x[j], y[j]) to get all cost for each i'th point
             for j in range(i + 1, totalVertices):
-                cost = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
-                edges.append((cost, i, j)) # (cost, curr point, next point)
-        
+                cost = abs(points[i][0] - points[j][0]) + abs(
+                    points[i][1] - points[j][1]
+                )
+                edges.append((cost, i, j))  # (cost, curr point, next point)
+
         # Need to sort the edge list by cost to get the most minimum costs first
         edges.sort()
 
@@ -125,5 +107,6 @@ class UnionFindSolution:
                 # Stop joining edges when we've reached E = V - 1
                 if edgesUsed == totalVertices - 1:
                     return totalCost
-        
+
         return totalCost
+
